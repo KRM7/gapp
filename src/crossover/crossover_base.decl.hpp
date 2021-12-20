67676bda43c9c58f1ff661ec5f1d718cff792ea3
KRM7 <70973547+KRM7@users.noreply.github.com>
2021-12-20 13:58:40 +0100

    added files for the base crossover class

---
+++ src/crossover/crossover_base.decl.hpp
@@ -0,0 +1,126 @@
+/*
+*  MIT License
+*
+*  Copyright (c) 2021 Krisztián Rugási
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*/
+
+
+#ifndef GA_CROSSOVER_BASE_DECL_HPP
+#define GA_CROSSOVER_BASE_DECL_HPP
+
+#include "../candidate.h"
+
+#include <vector>
+#include <utility>
+
+namespace genetic_algorithm
+{
+    template<typename GeneType>
+    class GA;
+}
+
+namespace genetic_algorithm::crossover
+{
+    /**
+    * Base class used for all of the crossover operators.
+    * Every implemented crossover operator takes 2 Candidates (the parents), and creates 2 children from these parents.
+    * The crossover operation is performed on the 2 parents with the set (pc) probability only, the rest of the time
+    * the returned children will be the same as the parents.
+    */
+    template<regular_hashable GeneType>
+    class Crossover
+    {
+    public:
+        /**
+        * Create a crossover operator that will use @p pc as the crossover rate.
+        *
+        * @param pc The crossover probability. Must be in the closed interval [0.0, 1.0].
+        */
+        explicit Crossover(double pc = 0.8);
+
+        virtual ~Crossover() = default;
+
+        /**
+        * Sets the crossover rate used in the algorithm to @p pc. 
+        *
+        * @param pc The crossover probability. Must be in the closed interval [0.0, 1.0].
+        */
+        void crossover_rate(double pc);
+
+        /** @returns The crossover rate currently set for this crossover operator. */
+        [[nodiscard]]
+        double crossover_rate() const noexcept { return pc_; }
+
+        /**
+        * Perform the crossover operation on 2 individuals with the set probability.
+        *
+        * @param ga The genetic algorithm the crossover operator is being used in.
+        * @param parent1 The first parent.
+        * @param parent2 The second parent.
+        * @returns The pair of children resulting from the crossover.
+        */
+        CandidatePair<GeneType> operator()(const GA<GeneType>& ga, const Candidate<GeneType>& parent1, const Candidate<GeneType>& parent2) const;
+
+    protected:
+
+        /* The actual crossover function. Performs the crossover every time and does nothing else. */
+        virtual CandidatePair<GeneType> crossover(const GA<GeneType>& ga, const Candidate<GeneType>& parent1, const Candidate<GeneType>& parent2) const = 0;
+
+        double pc_ = 0.8;   /* The probability of performing the crossover operation on the parents. */
+    };
+
+    /**
+    * Base class used for the crossover operators where each gene has a lower and upper bound.
+    * Used for the operators that perform crossover on real encoded chromosomes.
+    */
+    template<regular_hashable GeneType>
+    class BoundedCrossover : public Crossover<GeneType>
+    {
+    public:
+        /**
+        * Create a bounded crossover operator with @p bounds used as the bounds for the genes.
+        *
+        * @param bounds The (lower and upper) bounds of the genes. Must be the same length as the chromosomes used in the algorithm.
+        * @param pc The crossover probability.
+        */
+        BoundedCrossover(const std::vector<std::pair<GeneType, GeneType>>& bounds, double pc = 0.8);
+
+        /**
+        * Sets the boundaries of the genes.
+        * Each element of the @p bounds vector must contain the lower and upper bounds of the corresponding gene
+        * (the min and max values of the gene), with the lower bound never being greater than the upper bound for a gene.
+        * The number of elements in @p bounds must be the same as the length of the chromosomes used.
+        *
+        * @param bounds The (lower and upper) bounds of the genes.
+        */
+        void bounds(const std::vector<std::pair<GeneType, GeneType>>& bounds);
+
+        /** @returns The bounds currently set for this crossover operator. */
+        [[nodiscard]]
+        std::vector<std::pair<GeneType, GeneType>> bounds() const noexcept { return bounds_; }
+
+    protected:
+        std::vector<std::pair<GeneType, GeneType>> bounds_;     /* The lower and upper bounds for each gene of the chromosome. */
+    };
+
+} // namespace genetic_algorithm::crossover
+
+#endif // !GA_CROSSOVER_BASE_DECL_HPP
---
+++ src/crossover/crossover_base.impl.hpp
@@ -0,0 +1,135 @@
+/*
+*  MIT License
+*
+*  Copyright (c) 2021 Krisztián Rugási
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*/
+
+#ifndef GA_CROSSOVER_BASE_IMPL_HPP
+#define GA_CROSSOVER_BASE_IMPL_HPP
+
+#include "crossover_base.decl.hpp"
+#include "../rng.h"
+
+#include <algorithm>
+#include <stdexcept>
+#include <cassert>
+
+
+namespace genetic_algorithm::crossover
+{
+    template<regular_hashable GeneType>
+    inline Crossover<GeneType>::Crossover(double pc)
+    {
+        crossover_rate(pc);
+    }
+
+    template<regular_hashable GeneType>
+    inline void Crossover<GeneType>::crossover_rate(double pc)
+    {
+        if (!(0.0 <= pc && pc <= 1.0))
+        {
+            throw std::invalid_argument("The crossover probability must be in the closed range [0.0, 1.0]");
+        }
+
+        pc_ = pc;
+    }
+
+    template<regular_hashable T>
+    inline CandidatePair<T> Crossover<T>::operator()(const GA<T>& ga, const Candidate<T>& parent1, const Candidate<T>& parent2) const
+    {
+        assert(0.0 <= pc_ && pc_ <= 1.0);
+
+        /* Only need to perform the crossover with the set pc probability. Return early with (1 - pc) probability. */
+        if (rng::randomReal() >= pc_)
+        {
+            return { parent1, parent2 };
+        }
+
+        /*
+        * If the parents are the same, the crossover doesn't need to be performed.
+        * This assumes that with 2 parents that have the same chromosomes, the children would be the same
+        * as the parents, which is true for every crossover operator implemented, but
+        * could be an issue for user defined crossovers.
+        */
+        if (parent1 == parent2)
+        {
+            return { parent1, parent2 };
+        }
+
+        /* Perform the actual crossover. */
+        auto [child1, child2] = crossover(ga, parent1, parent2);
+
+        child1.is_evaluated = false;
+        child2.is_evaluated = false;
+
+        /*
+        * Check if either of the children are the same as one of the parents.
+        * (This can happen in edge cases even if the parents are different.)
+        * If one of the children are the same as one of the parents, then the fitness function
+        * evaluation for that child can be skipped (if the fitness function is the same) by assigning
+        * it the same fitness as the parent.
+        */
+        if (child1 == parent1)
+        {
+            child1.fitness = parent1.fitness;
+            child1.is_evaluated = true;
+        }
+        else if (child1 == parent2)
+        {
+            child1.fitness = parent2.fitness;
+            child1.is_evaluated = true;
+        }
+        if (child2 == parent1)
+        {
+            child2.fitness = parent1.fitness;
+            child2.is_evaluated = true;
+        }
+        else if (child2 == parent2)
+        {
+            child2.fitness = parent2.fitness;
+            child2.is_evaluated = true;
+        }
+
+        return { child1, child2 };
+    }
+
+
+    template<regular_hashable GeneType>
+    inline BoundedCrossover<GeneType>::BoundedCrossover(const std::vector<std::pair<GeneType, GeneType>>& bounds, double pc)
+        : Crossover<GeneType>(pc)
+    {
+        this->bounds(bounds);
+    }
+
+    template<regular_hashable GeneType>
+    inline void BoundedCrossover<GeneType>::bounds(const std::vector<std::pair<GeneType, GeneType>>& bounds)
+    {
+        if (std::any_of(bounds.begin(), bounds.end(), [](std::pair<double, double> bound) {return bound.first > bound.second; }))
+        {
+            throw std::invalid_argument("The lower bound must be lower than the upper bound for each gene.");
+        }
+
+        bounds_ = bounds;
+    }
+
+} // namespace genetic_algorithm::crossover
+
+#endif // !GA_CROSSOVER_BASE_IMPL_HPP
