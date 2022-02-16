1354d7109807c33b06e8acf8bd92382e854bbd72
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-02-16 23:34:06 +0100

    added selection class

--- src/selection/selection_base.hpp
+++ src/selection/selection_base.hpp
@@ -1,10 +1,41 @@
+/*
+*  MIT License
+*
+*  Copyright (c) 2021 Krisztián Rugási
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*/
+
 #ifndef GA_SELECTION_BASE_HPP
 #define GA_SELECTION_BASE_HPP
 
-#include "../base_ga.decl.hpp"
 #include "../concepts.hpp"
 
 #include <vector>
+#include <cstddef>
+
+namespace genetic_algorithm
+{
+    template<typename T>
+    class GA;
+
+} // namespace genetic_algorithm
 
 namespace genetic_algorithm::selection
 {
@@ -13,10 +44,11 @@ namespace genetic_algorithm::selection
     {
     public:
         Selection() = default;
+        Selection(const GA<T>& ga) {};
         virtual ~Selection() = default;
 
         virtual void prepare(const GA<T>& ga) = 0;
-        virtual void select(const GA<T>& ga) = 0;
+        virtual Candidate<T> select(const GA<T>& ga) = 0;
 
     private:
         std::vector<double> pdf_;   /* Discrete selection probability distribution function of the population. */
