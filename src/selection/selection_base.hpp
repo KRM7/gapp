c4d86e597ea30217a15ada59f7cfb23f32643b2b
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-20 17:24:50 +0100

    added selection base class

--- src/selection/selection_base.hpp
+++ src/selection/selection_base.hpp
@@ -1,26 +1,4 @@
-/*
-*  MIT License
-*
-*  Copyright (c) 2021 Krisztián Rugási
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the "Software"), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in all
-*  copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-*  SOFTWARE.
-*/
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
 
 #ifndef GA_SELECTION_BASE_HPP
 #define GA_SELECTION_BASE_HPP
@@ -32,22 +10,66 @@ namespace genetic_algorithm
 {
     template<typename T>
     class GA;
+}
 
-} // namespace genetic_algorithm
-
+/** Selection methods used in the algorithms. */
 namespace genetic_algorithm::selection
 {
+    /**
+    * Base class used for all of the selection methods.
+    * The selection methods define most parts of a genetic algorithm (eg. single-objective or multi-objective,
+    * how to create the next population etc.), and not just the method for selecting a candidate from the population.
+    */
     template<gene T>
     class Selection
     {
     public:
         Selection() = default;
-        Selection(const GA<T>& ga) {};
+        Selection(const GA<T>&) {};
         virtual ~Selection() = default;
 
+        /** 
+        * Initialize the selection method if needed.
+        * Called exactly once at start of the genetic algorithm after the initial population
+        * has already been created.
+        * 
+        * @param ga The genetic algorithm that uses the selection method.
+        */
+        virtual void init(const GA<T>&) {}
+
+        /**
+        * Prepare the selection method for the selections beforehand if neccesary. 
+        * Called exactly once every generation before the selections take place.
+        * 
+        * @param ga The genetic algorithm that uses the selection method.
+        * @param pop The current population of the algorithm.
+        */
         virtual void prepare(const GA<T>& ga, const Population<T>& pop) = 0;
+
+        /**
+        * Select a single Candidate from the population.
+        * Called (population_size) number of times in every generation.
+        * 
+        * @param ga The genetic algorithm that uses the selection method.
+        * @param pop The current population of the algorithm.
+        * @returns The selected Candidate.
+        */
         virtual Candidate<T> select(const GA<T>& ga, const Population<T>& pop) = 0;
-        virtual Population<T> nextPopulation(const GA<T>& ga, Population<T>& old_pop, CandidateVec<T>& children) const;
+
+        /**
+        * Select the Candidates of the next generation (next population) from the Candidates of the
+        * current population and the child population generated from the current population.
+        * Called once at the end of each generation.
+        * 
+        * The default implementation simply chooses the best (population_size) number of candidates
+        * from the combined current and child populations (assuming fitness maximization).
+        * 
+        * @param ga The genetic algorithm that uses the selection method.
+        * @param pop The current population of the algorithm.
+        * @param children The child population generated from the current population.
+        * @returns The next population of the algorithm.
+        */
+        virtual Population<T> nextPopulation(const GA<T>& ga, Population<T>& pop, Population<T>& children);
     };
 
 } // namespace genetic_algorithm::selection
@@ -63,22 +85,19 @@ namespace genetic_algorithm::selection
 namespace genetic_algorithm::selection
 {
     template<gene T>
-    inline Population<T> Selection<T>::nextPopulation(const GA<T>&, Population<T>& old_pop, CandidateVec<T>& children) const
+    inline Population<T> Selection<T>::nextPopulation(const GA<T>& ga, Population<T>& old_pop, Population<T>& children)
     {
-        assert(!children.empty());
-
-        size_t pop_size = old_pop.size();
         old_pop.insert(old_pop.end(), std::make_move_iterator(children.begin()),
                                       std::make_move_iterator(children.end()));
 
         assert(std::all_of(old_pop.begin(), old_pop.end(), [](const Candidate<T>& sol) { return sol.is_evaluated && sol.fitness.size() > 0; }));
 
-        std::partial_sort(old_pop.begin(), old_pop.begin() + pop_size, old_pop.end(),
+        std::partial_sort(old_pop.begin(), old_pop.begin() + ga.population_size(), old_pop.end(),
         [](const Candidate<T>& lhs, const Candidate<T>& rhs)
         {
             return detail::paretoCompareLess(rhs.fitness, lhs.fitness);
         });
-        old_pop.resize(pop_size);
+        old_pop.resize(ga.population_size());
 
         return old_pop;
     }
