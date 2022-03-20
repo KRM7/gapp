347d5e052892fb33ebd4f7869b0b6ad95a780d6d
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-20 17:29:14 +0100

    updated ga to use the new selections

--- src/base_ga.decl.hpp
+++ src/base_ga.decl.hpp
@@ -17,6 +17,11 @@
 
 namespace genetic_algorithm
 {
+    namespace selection
+    {
+        template<gene T>
+        class Selection;
+    }
     namespace crossover
     {
         template<gene T>
@@ -72,34 +77,6 @@ namespace genetic_algorithm
         using callbackFunction_t = std::function<void(const GA*)>;
 
         /**
-        * The type of the genetic algorithm used, depending on the problem type (single- or multi-objective optimization). \n
-        * Set the mode used with @ref mode.
-        */
-        enum class Mode
-        {
-            single_objective,           /**< Simple single-objective genetic algorithm. */
-            multi_objective_sorting,    /**< Non-dominated sorting genetic algorithm (NSGA-II) for multi-objective optimization. */
-            multi_objective_decomp      /**< NSGA-III algorithm for many-objective optimization. */
-        };
-
-        /**
-        * The possible selection methods used in the single-objective algorithm. \n
-        * Choose the selection method with @ref selection_method. \n
-        * If the custom option is selected, the function set with @ref setWeightCalcFunction will be used to calculate
-        * the selection weights of the candidates, and weight proportional selection will be used to select candidates. \n
-        * All of the methods work with negative fitness values.
-        */
-        enum class SogaSelection
-        {
-            roulette,          /**< Standard roulette selection adapted to also work with negative fitness values. No parameters. */
-            rank,              /**< Standard rank selection. @see rank_sel_min_w_, @see rank_sel_max_w_ */
-            tournament,        /**< Standard tournament selection. @see tournament_size */
-            sigma,             /**< Sigma fitness scaling. @see sigma_scale */
-            boltzmann,         /**< Standard Boltzmann selection. @see boltzmann_temps */
-            custom             /**< A user defined function is used to compute the selection probabilities. @see customCalcWeights */
-        };
-
-        /**
         * Should be set to false if the fitness function does not change over time. \n
         * (The fitness function will always return the same value for a given chromosome.) \n
         * Used to eliminate unnecesary fitness evaluations.
@@ -117,9 +94,9 @@ namespace genetic_algorithm
         * The repair function applied to each Candidate of the population after the mutations if it isn't a nullptr. \n
         * This can be used to perform local search after the mutations, implementing a memetic algorithm.
         */
-        repairFunction_t repairFunction = nullptr;
+        RepairFunction repairFunction = nullptr;
 
-        callbackFunction_t endOfGenerationCallback = nullptr;
+        CallbackFunction endOfGenerationCallback = nullptr;
 
         /**
         * Standard constructor for the GA.
@@ -127,7 +104,7 @@ namespace genetic_algorithm
         * @param chrom_len The length of the chromosomes (number of genes).
         * @param fitness_function The fitness function to find the maximum of.
         */
-        GA(size_t chrom_len, fitnessFunction_t fitness_function);
+        GA(size_t chrom_len, FitnessFunction fitness_function);
 
         virtual ~GA() = default;
 
@@ -154,16 +131,6 @@ namespace genetic_algorithm
         /** @returns A History object containing stats from each generation of the single objective genetic algorithm. */
         [[nodiscard]] const History& soga_history() const;
 
-        [[nodiscard]] size_t num_objectives() const;
-
-        /**
-        * Set the type of the problem/genetic algorithm that will be used (single-/multi-objective).
-        *
-        * @param mode The algorithm type to use. @see Mode
-        */
-        void mode(Mode mode);
-        [[nodiscard]] Mode mode() const;
-
         /**
         * Sets the length of the chromosomes (number of genes) of the Candidate solutions used in the algorithm to @p len. \n
         * The chromosome length must be at least 1.
@@ -181,68 +148,6 @@ namespace genetic_algorithm
         */
         void population_size(size_t size);
         [[nodiscard]] size_t population_size() const;
-
-        /**
-        * Sets the selection function used in the single-objective algorithm to @p f. \n
-        * The selection function set is ignored in the other algorithm types. @see Mode
-        *
-        * @param f The selection function used in the single-objective algorithm.
-        */
-        void selection_method(selectionFunction_t f);
-
-        /**
-        * Sets the selection method used in the single-objective algorithm to @p method. \n
-        * The selection method set is ignored in the other algorithm types. @see Mode
-        *
-        * @param method The selection method used in the single-objective algorithm.
-        */
-        void selection_method(SogaSelection method);
-        [[nodiscard]] SogaSelection selection_method() const;
-
-        /**
-        * Sets the number of solutions picked for the tournaments to @p size if the tournament selection method is
-        * selected for the single-objective algorithm. @see selection_method @see SogaSelection \n
-        * The size of the tournaments must be at least 2.
-        *
-        * @param size The size of the tournaments during tournament selection.
-        */
-        void tournament_size(size_t size);
-        [[nodiscard]] size_t tournament_size() const;
-
-        /**
-        * Sets the minimum and maximum selection weights used during rank selection if the rank selection method is
-        * selected for the single-objective algorithm. @see selection_method @see SogaSelection \n
-        * The @p min_weight must be on the closed interval [0.0, @p max_weight]. \n
-        * The @p max_weight must be greater than @p min_weight.
-        *
-        * @param min_weight The selection weight assigned to the worst Candidate in the population.
-        * @param max_weight The selection weight assigned to the best Candidate in the population.
-        */
-        void rank_sel_weights(double min_weight, double max_weight);
-        [[nodiscard]] std::pair<double, double> rank_sel_weights() const;
-
-        /**
-        * Sets the minimum and maximum temperature values used during boltzmann selection if the boltzmann
-        * selection method is selected for the single-objective algorithm. @see selection_method @see SogaSelection \n
-        * The minimum @p tmin temperature must be on the interval [0.1, @p tmax). \n
-        * The maximum @p tmax temperature must be on the interval (@p tmin, DBL_MAX).
-        *
-        * @param tmin The minimum temperature (used in the last generation).
-        * @param tmax The maximum temperature (used in the first generation).
-        */
-        void boltzmann_temps(double tmin, double tmax);
-        [[nodiscard]] std::pair<double, double> boltzmann_temps() const;
-
-        /**
-        * Sets the scaling parameter used during selection to @p scale if the sigma selection method is
-        * selected for the single-objective algorithm. @see selection_method @see SogaSelection \n
-        * The scaled fitness values are: f' = (f - f_mean) / (@p scale * f_sd) \n
-        * The value of @p scale must be on the interval [1.0, DBL_MAX].
-        *
-        * @param scale The scaling parameter used during sigma selection.
-        */
-        void sigma_scale(double scale);
-        [[nodiscard]] double sigma_scale() const;
 
         /**
         * Sets the maximum number of generations the algorithm runs for to @p max_gen. The
@@ -273,14 +178,13 @@ namespace genetic_algorithm
         *
         * @param f The fitness function to find the maximum of.
         */
-        void setFitnessFunction(fitnessFunction_t f);
+        void setFitnessFunction(FitnessFunction f);
 
-        /* Some getters for the NSGA-III algorithm. */
-        [[nodiscard]] std::vector<std::vector<double>> ref_points() const;
-        [[nodiscard]] std::vector<double> ideal_point() const;
-        [[nodiscard]] std::vector<double> nadir_point() const;
+        [[nodiscard]] size_t num_objectives() const noexcept;
+
 
         /* CROSSOVER */
+
         template<typename CrossoverType>
         //requires std::derived_from<CrossoverType, crossover::Crossover<GeneType>> && std::copy_constructible<CrossoverType>
         void crossover_method(const CrossoverType& f);
@@ -293,8 +197,9 @@ namespace genetic_algorithm
 
 
         /* MUTATION */
+
         template<typename MutationType>
-        //requires std::derived_from<MutationType, mutation::Mutation<GeneType>> && std::copy_constructible<MutationType>
+        //requires std::derived_from<MutationType, mutation::Mutation<GeneType>>&& std::copy_constructible<MutationType>
         void mutation_method(const MutationType& f);
 
         void mutation_method(std::unique_ptr<mutation::Mutation<GeneType>>&& f);
@@ -305,6 +210,7 @@ namespace genetic_algorithm
 
 
         /* STOP CONDITION */
+
         /* The algorithm always stops when the set max_gen generation has been reached regardless of the stop condition
         set. */
         template<typename StopType>
@@ -317,47 +223,48 @@ namespace genetic_algorithm
         //requires std::derived_from<StopType, stopping::StopCondition<GeneType>>
         StopType& stop_condition() const;
 
+
+        /* SELECTION METHOD */
+
+        /**
+        * Sets the selection method used in the single-objective algorithm to @p method. \n
+        * The selection method set is ignored in the other algorithm types. @see Mode
+        *
+        * @param method The selection method used in the single-objective algorithm.
+        */
+        template<typename SelectionType>
+        //requires std::derived_from<SelectionType, selection::Selection<GeneType>> && std::copy_constructible<SelectionType>
+        void selection_method(const SelectionType& f);
+
+        void selection_method(std::unique_ptr<selection::Selection<GeneType>>&& f);
+
+        template<typename SelectionType = selection::Selection<GeneType>>
+        SelectionType& selection_method() const;
+
     protected:
 
         Population population_;
         size_t generation_cntr_ = 0;
         size_t num_objectives_ = 0;        /* Determined from the fitness function. */
 
-        /* For the NSGA-III. */
-        std::vector<std::vector<double>> ref_points_;
-        std::vector<double> ideal_point_;
-        std::vector<double> nadir_point_;
-        std::vector<std::vector<double>> extreme_points_;
-
-        /* Results of the GA. */
+        /* Results. */
         CandidateVec solutions_;
         std::atomic<size_t> num_fitness_evals_ = 0;
         History soga_history_;
 
         /* Basic parameters of the GA. */
-        Mode mode_ = Mode::single_objective;
         size_t chrom_len_;
         size_t population_size_ = 100;
-
-        /* Single-objective GA selection settings. */
-        SogaSelection selection_method_ = SogaSelection::tournament;
-        size_t tournament_size_ = 2;
-        double rank_sel_min_w_ = 0.1;
-        double rank_sel_max_w_ = 1.1;
-        double boltzmann_tmin_ = 0.25;
-        double boltzmann_tmax_ = 4.0;
-        double sigma_scale_ = 3.0;
-
-        /* Stop condition settings. */
         size_t max_gen_ = 500;
 
         /* Initial population settings. */
         Population initial_population_preset_;
 
         /* User supplied functions used in the GA. All of these are optional except for the fitness function. */
-        fitnessFunction_t fitnessFunction;
-        selectionFunction_t customSelection = nullptr;
+        FitnessFunction fitnessFunction;
+        //selectionFunction_t customSelection = nullptr;
 
+        std::unique_ptr<selection::Selection<GeneType>> selection_;
         std::unique_ptr<crossover::Crossover<GeneType>> crossover_;
         std::unique_ptr<mutation::Mutation<GeneType>> mutation_;
         std::unique_ptr<stopping::StopCondition<GeneType>> stop_condition_;
@@ -371,71 +278,9 @@ namespace genetic_algorithm
         bool stopCondition();
         void evaluate(Population& pop);
         void updateOptimalSolutions(CandidateVec& optimal_sols, const Population& pop) const;
-        void prepSelections(Population& pop) const;
-        Candidate select(const Population& pop) const;
         void repair(Population& pop) const;
-        Population updatePopulation(Population& old_pop, CandidateVec& children);
         void updateStats(const Population& pop);
 
-
-        /* SOGA functions. */
-
-        /* Functions for calculating the selection probabilities of individuals in the single-objective algorithm. */
-
-        static void sogaCalcRouletteWeights(Population& pop);
-        static void sogaCalcRankWeights(Population& pop, double weight_min = 0.1, double weight_max = 1.1);
-        static void sogaCalcSigmaWeights(Population& pop, double scale = 3.0);
-        static void sogaCalcBoltzmannWeights(Population& pop, size_t t, size_t t_max, double temp_min, double temp_max);
-
-        void sogaCalcWeights(Population& pop) const;
-
-        /* Functions used for the selections in the single-objective algorithm. */
-
-        static Candidate sogaWeightProportionalSelect(const Population& pop);
-        static Candidate sogaTournamentSelect(const Population& pop, size_t tourney_size);
-
-        Candidate sogaSelect(const Population& pop) const;
-
-        /* Create the population of the next generation from the old population and the children. */
-        Population updateSogaPopulation(Population& old_pop, CandidateVec& children) const;
-
-
-        /* NSGA-II functions. */
-
-        /* Find all Pareto fronts in the population and also assign the nondomination ranks of the candidates (assuming fitness maximization). */
-        static std::vector<std::vector<size_t>> nonDominatedSort(Population& pop);
-
-        /* Calculate the crowding distances of the candidates in each pareto front in pfronts of the population. */
-        static void calcCrowdingDistances(Population& pop, std::vector<std::vector<size_t>>& pfronts);
-
-        /* Returns true if lhs is better than rhs. */
-        static bool crowdedCompare(const Candidate& lhs, const Candidate& rhs);
-
-        static Candidate nsga2Select(const Population& pop);
-
-        /* Create the population of the next generation from the old population and the children. */
-        Population updateNsga2Population(Population& old_pop, CandidateVec& children) const;
-
-
-        /* NSGA-III functions. */
-
-        void updateIdealPoint(const Population& pop);
-        void updateNadirPoint(const Population& pop);
-
-        /* Find the closest reference point to each candidate after normalization, and their distances. */
-        void associatePopToRefs(Population& pop, const std::vector<std::vector<double>>& ref_points);
-
-        /* Return the niche counts of the ref points and assign niche counts to the candidates. */
-        static std::vector<size_t> calcNicheCounts(Population& pop, const std::vector<std::vector<double>>& ref_points);
-
-        /* Returns true if lhs is better than rhs. */
-        static bool nichedCompare(const Candidate& lhs, const Candidate& rhs);
-
-        /* Tournament selection using the niche counts for tiebreaks. */
-        static Candidate nsga3Select(const Population& pop);
-
-        /* Create the population of the next generation from the old population and the children. */
-        Population updateNsga3Population(Population& old_pop, CandidateVec& children);
     };
 
 } // namespace genetic_algorithm
