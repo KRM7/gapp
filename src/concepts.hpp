96e3b82ded710f5cc83f68a9596c0f341ab18d6e
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-27 00:38:45 +0100

    added container concepts

--- src/concepts.hpp
+++ src/concepts.hpp
@@ -28,6 +28,8 @@
 #include <concepts>
 #include <functional>
 #include <type_traits>
+#include <limits>
+#include <iterator>
 
 namespace genetic_algorithm
 {
@@ -80,6 +82,66 @@ namespace genetic_algorithm
     /** Genetic algorithm types. (Types that are derived from the GA class.) */
     template<typename T>
     concept genetic_algorithm = detail::derived_from_specialization_of<T, GA>;
+
+    /* based on named requirements */
+    template<typename C>
+    concept Container = requires(C c, const C cc, C&& rc)
+    {
+        requires C::value_type;
+        requires C::reference;
+        requires C::const_reference;
+        requires C::iterator;
+        requires C::const_iterator;
+        requires C::difference_type;
+        requires C::size_type;
+
+        requires std::same_as<typename C::reference, typename C::value_type&>;
+        requires std::same_as<typename C::const_reference, const typename C::value_type&>;
+
+        requires std::forward_iterator<typename C::iterator>;
+        requires std::same_as<typename std::iterator_traits<typename C::iterator>::value_type, typename C::value_type>;
+        requires std::forward_iterator<typename C::const_iterator>;
+        requires std::same_as<typename std::iterator_traits<typename C::const_iterator>::value_type, const typename C::value_type>;
+
+        requires std::signed_integral<typename C::difference_type>;
+        requires std::same_as<typename C::difference_type, typename std::iterator_traits<typename C::iterator>::difference_type>;
+        requires std::same_as<typename C::difference_type, typename std::iterator_traits<typename C::const_iterator>::difference_type>;
+        requires std::unsigned_integral<typename C::size_type>;
+        requires std::numeric_limits<typename C::size_type>::max() >= std::numeric_limits<typename C::difference_type>::max();
+
+        requires std::destructible<typename C::value_type>;
+        requires std::regular<C>;
+        requires std::destructible<C>;
+        
+        { c.operator=(c) } -> std::same_as<C&>;
+        { c.operator=(cc) } -> std::same_as<C&>;
+        { c.operator=(rc) } -> std::same_as<C&>;
+
+        { c.begin() } -> std::same_as<typename C::iterator>;
+        { cc.begin() } -> std::same_as<typename C::const_iterator>;
+        { c.end() } -> std::same_as<typename C::iterator>;
+        { cc.end() } -> std::same_as<typename C::const_iterator>;
+        { c.cbegin() } -> std::same_as<typename C::const_iterator>;
+        { c.cbegin() } -> std::same_as<typename C::const_iterator>;
+        { cc.cbegin() } -> std::same_as<typename C::const_iterator>;
+        { cc.cbegin() } -> std::same_as<typename C::const_iterator>;
+
+        { c.size() } -> std::same_as<typename C::size_type>;
+        { c.max_size() } -> std::same_as<typename C::size_type>;
+        { cc.size() } -> std::same_as<typename C::size_type>;
+        { cc.max_size() } -> std::same_as<typename C::size_type>;
+
+        { c.empty() } -> std::convertible_to<bool>;
+        { cc.empty() } -> std::convertible_to<bool>;
+    };
+
+    template<typename C>
+    concept IndexableContainer = requires(C c, const C cc, size_t idx)
+    {
+        requires Container<C>;
+        { c.operator[](idx) } -> std::same_as<typename C::reference>;
+        { cc.operator[](idx) } -> std::same_as<typename C::const_reference>;
+    };
 
 } // namespace genetic_algorithm
 
