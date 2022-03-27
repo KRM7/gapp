5841375541fdd274d01562263455d29be10572ad
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-27 22:52:31 +0200

    moved math files to utility

---
+++ src/utility/math.cpp
@@ -0,0 +1,119 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#include "math.hpp"
+#include "algorithm.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <cstddef>
+#include <cassert>
+
+namespace genetic_algorithm::detail
+{
+    constexpr double square(double n) noexcept
+    {
+        return n * n;
+    }
+
+    bool floatIsEqual(double lhs, double rhs, double eps) noexcept
+    {
+        assert(0.0 <= eps && eps <= 1.0);
+
+        return std::abs(lhs - rhs) <= std::max(std::abs(lhs), std::abs(rhs)) * eps;
+    }
+
+    bool floatIsLess(double lhs, double rhs, double eps) noexcept
+    {
+        assert(0.0 <= eps && eps <= 1.0);
+
+        return (rhs - lhs) > std::max(std::abs(lhs), std::abs(rhs)) * eps;
+    }
+
+    bool floatIsEqual(const std::vector<double>& lhs, const std::vector<double>& rhs, double eps) noexcept
+    {
+        assert(0.0 <= eps && eps <= 1.0);
+
+        return (lhs.size() == rhs.size()) &&
+               std::equal(lhs.begin(), lhs.end(), rhs.begin(), [eps](double lhs, double rhs) { return floatIsEqual(lhs, rhs, eps); });
+    }
+
+    bool paretoCompareLess(const std::vector<double>& lhs, const std::vector<double>& rhs, double eps)
+    {
+        assert(0.0 <= eps && eps <= 1.0);
+        assert(lhs.size() == rhs.size());
+
+        bool has_lower = false;
+        for (size_t i = 0; i < lhs.size(); i++)
+        {
+            if (floatIsLess(rhs[i], lhs[i], eps)) return false;
+            if (floatIsLess(lhs[i], rhs[i], eps)) has_lower = true;
+        }
+
+        return has_lower;
+    }
+
+    double euclideanDistanceSq(const std::vector<double>& v1, const std::vector<double>& v2)
+    {
+        assert(v1.size() == v2.size());
+
+        return std::transform_reduce(v1.begin(), v1.end(), v2.begin(), 0.0,
+                                     std::plus<double>{},
+                                     compose(std::minus<double>{}, square));
+    }
+
+    double perpendicularDistanceSq(const std::vector<double>& line, const std::vector<double>& point)
+    {
+        assert(line.size() == point.size());
+        assert(!line.empty());
+
+        double k = std::inner_product(line.begin(), line.end(), point.begin(), 0.0) /
+                   std::inner_product(line.begin(), line.end(), line.begin(), 0.0);
+
+        return std::transform_reduce(point.begin(), point.end(), line.begin(), 0.0,
+                                     std::plus<double>(),
+                                     [k](double p, double l) { return square(p - k * l); });
+    }
+
+    double mean(const std::vector<double>& vec) noexcept
+    {
+        assert(!vec.empty());
+
+        return std::accumulate(vec.begin(), vec.end(), 0.0,
+        [n = vec.size()](double acc, double val)
+        {
+            return acc + val / n;
+        });
+    }
+
+    double stdDev(const std::vector<double>& vec) noexcept
+    {
+        assert(!vec.empty());
+
+        if (vec.size() == 1) return 0.0;
+
+        auto var = std::accumulate(vec.begin(), vec.end(), 0.0L,
+        [avg = mean(vec), n = vec.size()](long double acc, double val)
+        {
+            return acc + square(val - avg) / (n - 1.0);
+        });
+
+        return double(std::sqrt(var));
+    }
+
+    double stdDev(const std::vector<double>& vec, double mean) noexcept
+    {
+        assert(!vec.empty());
+
+        if (vec.size() == 1) return 0.0;
+
+        auto var = std::accumulate(vec.begin(), vec.end(), 0.0L,
+        [mean, n = vec.size()](long double acc, double val)
+        {
+            return acc + square(val - mean) / (n - 1.0);
+        });
+
+        return double(std::sqrt(var));
+    }
+}
---
+++ src/utility/math.hpp
@@ -0,0 +1,40 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_MATH_HPP
+#define GA_MATH_HPP
+
+#include "utils.hpp"
+
+#include <vector>
+
+namespace genetic_algorithm::detail
+{
+    /* Equality comparison for floating point numbers. Returns true if lhs is approximately equal to rhs. */
+    bool floatIsEqual(double lhs, double rhs, double eps = GA_EPSILON) noexcept;
+
+    /* Less than comparison for floating point numbers. Returns true if lhs is definitely less than rhs. */
+    bool floatIsLess(double lhs, double rhs, double eps = GA_EPSILON) noexcept;
+
+    /* Equality comparison for fp vectors. Returns true if the elements of the vectors are approximately equal. */
+    bool floatIsEqual(const std::vector<double>& lhs, const std::vector<double>& rhs, double eps = GA_EPSILON) noexcept;
+
+    /* Pareto comparison for fp vectors. Returns true if lhs is dominated by rhs (lhs < rhs) assuming maximization. */
+    bool paretoCompareLess(const std::vector<double>& lhs, const std::vector<double>& rhs, double eps = GA_EPSILON);
+
+    /* Calculate the square of the Euclidean distance between the vectors v1 and v2. */
+    double euclideanDistanceSq(const std::vector<double>& v1, const std::vector<double>& v2);
+
+    /* Calculate the square of the perpendicular distance between a line and a point. */
+    double perpendicularDistanceSq(const std::vector<double>& line, const std::vector<double>& point);
+
+    /* Calculate the arithmetic mean of the values in vec. */
+    double mean(const std::vector<double>& vec) noexcept;
+
+    /* Calculate the standard deviation of the values in vec. */
+    double stdDev(const std::vector<double>& vec) noexcept;
+
+    /* Calculate the standard deviation of the values in vec. */
+    double stdDev(const std::vector<double>& vec, double mean) noexcept;
+}
+
+#endif // !GA_MATH_HPP
