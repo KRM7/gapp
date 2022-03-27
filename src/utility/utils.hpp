c538ca9b90dd7447822cc3d2cdcaf53d65b2659e
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-03-27 22:53:32 +0200

    moved utils to utility

---
+++ src/utility/utils.hpp
@@ -0,0 +1,33 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_UTILS_HPP
+#define GA_UTILS_HPP
+
+#include <cstdlib>
+#include <cassert>
+#include <limits>
+#include <execution>
+#include <random>
+
+
+#define GA_UNREACHABLE (assert(!"Unreachable code."), std::terminate())
+
+#ifndef GA_EPSILON
+#define GA_EPSILON (100 * std::numeric_limits<double>::epsilon())
+#endif
+
+#ifndef GA_SEQ_EXECUTION
+#define GA_EXECUTION_UNSEQ std::execution::par_unseq
+#define GA_EXECUTION_SEQ std::execution::par
+#else
+#define GA_EXECUTION_UNSEQ std::execution::unseq
+#define GA_EXECUTION_SEQ std::execution::seq
+#endif
+
+#ifndef GA_PRESET_SEED
+#define GA_SEED() (std::random_device{}())
+#else
+#define GA_SEED() (0x12345678);
+#endif
+
+#endif // !GA_UTILS_HPP
