dd404bf0683cd9bc38e71fa9f2258f4c0a57cd1f
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-04-10 16:50:50 +0200

    separate single- multi-objective selections

---
+++ src/selection/multi_objective.hpp
@@ -0,0 +1,78 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_SELECTION_MULTI_OBJECTIVE_HPP
+#define GA_SELECTION_MULTI_OBJECTIVE_HPP
+
+#include "selection_base.hpp"
+#include <vector>
+#include <cstddef>
+
+namespace genetic_algorithm::selection::multi_objective
+{
+    /**
+    * Non-dominated sorting genetic algorithm (NSGA-II) for multi-objective optimization.
+    */
+    class NSGA2 final : public Selection
+    {
+    public:
+        void init(const GaInfo& ga) override;
+        void prepare(const GaInfo& ga, const FitnessMatrix& pop) override;
+        size_t select(const GaInfo& ga, const FitnessMatrix& pop) override;
+        std::vector<size_t> nextPopulation(const GaInfo& ga, FitnessMatrix& combined_pop) override;
+
+    private:
+        std::vector<size_t> ranks_;
+        std::vector<double> dists_;
+
+        /* Returns true if Pop[lidx] is better than Pop[ridx]. */
+        bool crowdedCompare(size_t lidx, size_t ridx) const;
+    };
+
+    /**
+    * NSGA-III algorithm for many-objective optimization.
+    */
+    class NSGA3 final : public Selection
+    {
+    public:
+        void init(const GaInfo& ga) override;
+        void prepare(const GaInfo& ga, const FitnessMatrix& pop) override;
+        size_t select(const GaInfo& ga, const FitnessMatrix& pop) override;
+        std::vector<size_t> nextPopulation(const GaInfo& ga, FitnessMatrix& combined_pop) override;
+
+    private:
+        using Point = std::vector<double>;
+
+        struct CandidateInfo
+        {
+            size_t rank = 0;
+            size_t ref_idx = 0;
+            double ref_dist = 0.0;
+            size_t niche_count = 0;
+        };
+
+        std::vector<CandidateInfo> sol_props_;
+        std::vector<Point> ref_points_;
+        std::vector<size_t> ref_niche_counts_;
+
+        Point ideal_point_;
+        Point nadir_point_;
+        std::vector<Point> extreme_points_;
+
+        static void updateIdealPoint(Point& ideal_point, const FitnessMatrix& pop);
+        static std::vector<Point> initExtremePoints(const FitnessMatrix& pop, const Point& ideal_point);
+        static void updateExtremePoints(std::vector<Point>& extreme_points, const FitnessMatrix& pop, const Point& ideal_point);
+        static Point nadirPoint(const std::vector<Point>& extreme_points);
+
+        /* Find the closest reference point to each candidate after normalization, and their distances. */
+        void associatePopWithRefs(const FitnessMatrix& pop);
+
+        /* Return the niche counts of the ref points and assign niche counts to the candidates. */
+        static std::vector<size_t> calcNicheCounts(const GaInfo& ga, std::vector<CandidateInfo>& props);
+
+        /* Returns true if Pop[lidx] is better than Pop[ridx]. */
+        bool nichedCompare(size_t lidx, size_t ridx) const;
+    };
+
+} // namespace genetic_algorithm::selection::multi_objective
+
+#endif // !GA_SELECTION_MULTI_OBJECTIVE_HPP
---
+++ src/selection/single_objective.cpp
@@ -0,0 +1,143 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#include "single_objective.hpp"
+#include "../algorithms/ga_info.hpp"
+#include "../utility/rng.hpp"
+#include <limits>
+#include <utility>
+#include <stdexcept>
+#include <cassert>
+
+namespace genetic_algorithm::selection::single_objective
+{
+    void Roulette::prepare(const GaInfo&, const FitnessMatrix& pop)
+    {
+        auto selection_weights = dtl::rouletteWeights(pop);
+
+        cdf_ = dtl::weightsToCdf(selection_weights);
+    }
+
+    size_t Roulette::select(const GaInfo&, const FitnessMatrix&)
+    {
+        return rng::sampleCdf(cdf_);
+    }
+
+    Tournament::Tournament(size_t size)
+    {
+        this->size(size);
+    }
+
+    void Tournament::size(size_t size)
+    {
+        if (size < 2)
+        {
+            throw std::invalid_argument("The tournament size must be at least 2.");
+        }
+
+        tourney_size_ = size;
+    }
+
+    void Tournament::prepare(const GaInfo&, const FitnessMatrix&)
+    { /* Nothing to do for tournament selection. */
+    }
+
+    size_t Tournament::select(const GaInfo&, const FitnessMatrix& pop)
+    {
+        assert(pop.size() >= tourney_size_);
+        assert(std::all_of(pop.begin(), pop.end(), [](const FitnessVector& sol) { return sol.size() == 1; }));
+
+        auto candidates = rng::sampleUnique(pop.size(), tourney_size_);
+
+        return *std::max_element(candidates.begin(), candidates.end(),
+        [&pop](size_t lidx, size_t ridx)
+        {
+            return pop[lidx][0] < pop[ridx][0];
+        });
+    }
+
+    Rank::Rank(double min_weight, double max_weight)
+    {
+        this->weights(min_weight, max_weight);
+    }
+
+    void Rank::min_weight(double min_weight)
+    {
+        this->weights(min_weight, max_weight_);
+    }
+
+    void Rank::max_weight(double max_weight)
+    {
+        this->weights(min_weight_, max_weight);
+    }
+
+    void Rank::weights(double min_weight, double max_weight)
+    {
+        if (!(0.0 <= min_weight && min_weight <= max_weight))
+        {
+            throw std::invalid_argument("The minimum weight must be in the closed interval [0.0, max_weight].");
+        }
+        if (!(min_weight <= max_weight && max_weight <= std::numeric_limits<double>::max()))
+        {
+            throw std::invalid_argument("The maximum weight must be in the closed interval [min_weight, DBL_MAX].");
+        }
+
+        min_weight_ = min_weight;
+        max_weight_ = max_weight;
+    }
+
+    void Rank::prepare(const GaInfo&, const FitnessMatrix& pop)
+    {
+        auto selection_weights = dtl::rankWeights(pop, min_weight_, max_weight_);
+
+        cdf_ = dtl::weightsToCdf(selection_weights);
+    }
+
+    size_t Rank::select(const GaInfo&, const FitnessMatrix&)
+    {
+        return rng::sampleCdf(cdf_);
+    }
+
+    Sigma::Sigma(double scale)
+    {
+        this->scale(scale);
+    }
+
+    void Sigma::scale(double scale)
+    {
+        if (!(1.0 <= scale && scale <= std::numeric_limits<double>::max()))
+        {
+            throw std::invalid_argument("Scale must be in the closed interval [1.0, DBL_MAX].");
+        }
+
+        scale_ = scale;
+    }
+
+    void Sigma::prepare(const GaInfo&, const FitnessMatrix& pop)
+    {
+        auto selection_weights = dtl::sigmaWeights(pop, scale_);
+
+        cdf_ = dtl::weightsToCdf(selection_weights);
+    }
+
+    size_t Sigma::select(const GaInfo&, const FitnessMatrix&)
+    {
+        return rng::sampleCdf(cdf_);
+    }
+
+    Boltzmann::Boltzmann(TemperatureFunction f) : temperature_(std::move(f))
+    {
+    }
+
+    void Boltzmann::prepare(const GaInfo& ga, const FitnessMatrix& pop)
+    {
+        double T = temperature_(ga.generation_cntr(), ga.max_gen());
+        auto selection_weights = dtl::boltzmannWeights(pop, T);
+
+        cdf_ = dtl::weightsToCdf(selection_weights);
+    }
+
+    size_t Boltzmann::select(const GaInfo&, const FitnessMatrix&)
+    {
+        return rng::sampleCdf(cdf_);
+    }
+} // namespace genetic_algorithm::selection::single_objective
