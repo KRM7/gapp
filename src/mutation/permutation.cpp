6bebf664e20808c0d5c28705eb7f4c4db46cf053
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-04-29 23:50:48 +0200

    updated perm mutations

--- src/mutation/permutation.cpp
+++ src/mutation/permutation.cpp
@@ -9,72 +9,142 @@
 
 namespace genetic_algorithm::mutation::perm
 {
-    void Inversion::mutate(const GaInfo&, Candidate<size_t>& candidate) const
+    Inversion::Inversion(double pm, double range_max)
+        : Mutation<GeneType>(pm)
     {
-        if (candidate.chromosome.size() > 1 && rng::randomReal() < pm_)
+        this->range_max(range_max);
+    }
+
+    void Inversion::range_max(double rm)
+    {
+        if (!(0.0 <= rm && rm <= 1.0))
         {
-            size_t len = rng::randomInt(size_t{ 2 }, std::max(size_t(0.75 * candidate.chromosome.size()), size_t{ 2 }));
-            size_t first = rng::randomInt<size_t>(0, candidate.chromosome.size() - len);
-            size_t last = first + len;
+            throw std::invalid_argument("The range_max parameter must be in the closed interval [0.0, 1.0].");
+        }
+
+        range_max_ = rm;
+    }
+
+    void Inversion::mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const
+    {
+        size_t chrom_len = candidate.chromosome.size();
+
+        if (chrom_len < 2) return;
+
+        if (rng::randomReal() < pm_)
+        {
+            size_t min_len = 2;
+            size_t max_len = std::max(size_t(range_max_ * chrom_len), min_len);
+            size_t range_len = rng::randomInt(min_len, max_len);
+
+            size_t first = rng::randomInt<size_t>(0, chrom_len - range_len);
+            size_t last = first + range_len;
 
             std::reverse(candidate.chromosome.begin() + first, candidate.chromosome.begin() + last);
         }
     }
 
-    void Swap2::mutate(const GaInfo&, Candidate<size_t>& candidate) const
+    void Swap2::mutate(const GaInfo&, Candidate<GeneType>& candidate) const
     {
-        if (candidate.chromosome.size() < 2)
-        {
-            throw std::invalid_argument("The chromosome must have at least 2 genes for the Swap2 mutation.");
-        }
+        if (candidate.chromosome.size() < 2) return;
 
         if (rng::randomReal() < pm_)
         {
             auto idxs = rng::sampleUnique(candidate.chromosome.size(), 2);
-            std::swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[1]]);
+
+            using std::swap;
+            swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[1]]);
         }
     }
 
-    void Swap3::mutate(const GaInfo&, Candidate<size_t>& candidate) const
+    void Swap3::mutate(const GaInfo&, Candidate<GeneType>& candidate) const
     {
-        if (candidate.chromosome.size() < 3)
-        {
-            throw std::invalid_argument("The chromosome must have at least 3 genes for the Swap3 mutation.");
-        }
+        if (candidate.chromosome.size() < 3) return;
 
         if (rng::randomReal() < pm_)
         {
             auto idxs = rng::sampleUnique(candidate.chromosome.size(), 3);
-            std::swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[1]]);
-            std::swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[2]]);
+
+            using std::swap;
+            swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[1]]);
+            swap(candidate.chromosome[idxs[0]], candidate.chromosome[idxs[2]]);
         }
     }
 
-    void Shuffle::mutate(const GaInfo&, Candidate<size_t>& candidate) const
+    Shuffle::Shuffle(double pm, double range_max)
+        : Mutation<GeneType>(pm)
     {
-        if (candidate.chromosome.size() > 1 && rng::randomReal() < pm_)
+        this->range_max(range_max);
+    }
+
+    void Shuffle::range_max(double rm)
+    {
+        if (!(0.0 <= rm && rm <= 1.0))
         {
-            size_t len = rng::randomInt(size_t{ 2 }, std::max(size_t(0.5 * candidate.chromosome.size()), size_t{ 2 }));
-            size_t first = rng::randomInt<size_t>(0, candidate.chromosome.size() - len);
-            size_t last = first + len;
+            throw std::invalid_argument("The range_max parameter must be in the closed interval [0.0, 1.0].");
+        }
+
+        range_max_ = rm;
+    }
+
+    void Shuffle::mutate(const GaInfo&, Candidate<GeneType>& candidate) const
+    {
+        size_t chrom_len = candidate.chromosome.size();
+
+        if (chrom_len < 2) return;
+
+        if (rng::randomReal() < pm_)
+        {
+            size_t min_len = 2;
+            size_t max_len = std::max(size_t(range_max_ * chrom_len), min_len);
+            size_t range_len = rng::randomInt(min_len, max_len);
+
+            size_t first = rng::randomInt<size_t>(0, chrom_len - range_len);
+            size_t last = first + range_len;
 
             std::shuffle(candidate.chromosome.begin() + first, candidate.chromosome.begin() + last, rng::prng);
         }
     }
 
-    void Shift::mutate(const GaInfo&, Candidate<size_t>& candidate) const
+    Shift::Shift(double pm, double range_max)
+        : Mutation<GeneType>(pm)
     {
-        if (candidate.chromosome.size() > 1 && rng::randomReal() < pm_)
-        {
-            size_t len = rng::randomInt(size_t{ 2 }, std::max(size_t(0.75 * candidate.chromosome.size()), size_t{ 2 }));
-            size_t first = rng::randomInt<size_t>(0, candidate.chromosome.size() - len);
-            size_t last = first + len;
+        this->range_max(range_max);
+    }
 
-            std::vector<size_t> moved_elements(candidate.chromosome.begin() + first, candidate.chromosome.begin() + last);
-            candidate.chromosome.erase(candidate.chromosome.begin() + first, candidate.chromosome.begin() + last);
+    void Shift::range_max(double rm)
+    {
+        if (!(0.0 <= rm && rm <= 1.0))
+        {
+            throw std::invalid_argument("The range_max parameter must be in the closed interval [0.0, 1.0].");
+        }
+
+        range_max_ = rm;
+    }
+
+    void Shift::mutate(const GaInfo&, Candidate<GeneType>& candidate) const
+    {
+        size_t chrom_len = candidate.chromosome.size();
+
+        if (chrom_len < 2) return;
+
+        if (rng::randomReal() < pm_)
+        {
+            size_t min_len = 2;
+            size_t max_len = std::max(size_t(range_max_ * chrom_len), min_len);
+            size_t range_len = rng::randomInt(min_len, max_len);
+            size_t first = rng::randomInt<size_t>(0, chrom_len - range_len);
+            size_t last = first + range_len;
+
+            std::vector<GeneType> moved_elements(std::make_move_iterator(candidate.chromosome.begin()) + first,
+                                                 std::make_move_iterator(candidate.chromosome.begin()) + last);
+            candidate.chromosome.erase(candidate.chromosome.begin() + first,
+                                       candidate.chromosome.begin() + last);
 
             size_t new_pos = rng::randomInt(size_t{ 0 }, candidate.chromosome.size());
-            candidate.chromosome.insert(candidate.chromosome.begin() + new_pos, moved_elements.begin(), moved_elements.end());
+            candidate.chromosome.insert(candidate.chromosome.begin() + new_pos,
+                                        std::make_move_iterator(moved_elements.begin()),
+                                        std::make_move_iterator(moved_elements.end()));
         }
     }
 
--- src/mutation/permutation.hpp
+++ src/mutation/permutation.hpp
@@ -9,20 +9,43 @@
 namespace genetic_algorithm::mutation::perm
 {
     /**
-    * Inversion mutation operator for the permutation encoded genetic algorithm.
+    * Inversion mutation operator for the permutation genetic algorithm. \n
     * Each individual is mutated with the specified mutation probability.
-    * In the mutated individuals, a random range of genes is chosen and reversed.
+    * In the mutated individuals, a randomly selected range of genes are reversed.
     */
     class Inversion : public Mutation<size_t>
     {
     public:
-        using Mutation::Mutation;
+        /**
+        * Create an inversion mutation operator.
+        * 
+        * @param pm The mutation probability. Must be in the closed interval [0.0, 1.0].
+        * @param range_max The value of the range max parameter. Must be in the closed interval [0.0, 1.0].
+        */
+        explicit Inversion(double pm, double range_max = 0.75);
+
+        /**
+        * Set the value of the maximum range length parameter for the operator. \n
+        * The parameter determines the maximum length of the reversed range of genes during mutation:
+        * the maximum length is @p rm * chrom_len. \n
+        * The value of this parameter must be in the closed interval [0.0, 1.0].
+        * 
+        * @param rm The value of the range max parameter.
+        */
+        void range_max(double rm);
+
+        /** @ returns The current value of the range_max parameter. */
+        [[nodiscard]]
+        double range_max() const noexcept { return range_max_; }
+
     private:
-        void mutate(const GaInfo& ga, Candidate<size_t>& candidate) const override;
+        void mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const override;
+
+        double range_max_ = 0.75;
     };
 
     /**
-    * Single swap/swap2 mutation operator for the permutation encoded genetic algorithm.
+    * Single swap/swap2 mutation operator for the permutation genetic algorithm. \n
     * Each individual is mutated with the set mutation probability.
     * Two randomly selected genes are swapped in the mutated individuals.
     */
@@ -31,37 +54,60 @@ namespace genetic_algorithm::mutation::perm
     public:
         using Mutation::Mutation;
     private:
-        void mutate(const GaInfo& ga, Candidate<size_t>& candidate) const override;
+        void mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const override;
     };
 
     /**
-    * Swap-3 mutation operator for the permutation encoded genetic algorithm.
+    * Swap-3 mutation operator for the permutation genetic algorithm. \n
     * Each individual is mutated with the set mutation probability.
-    * Three randomly selected genes are swapped in the mutated individuals (0-1-2)->(2-0-1).
+    * In the mutated candidates, 3 randomly selected genes are reordered as: (a-b-c) -> (c-a-b).
     */
     class Swap3 final : public Mutation<size_t>
     {
     public:
         using Mutation::Mutation;
     private:
-        void mutate(const GaInfo& ga, Candidate<size_t>& candidate) const override;
+        void mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const override;
     };
 
     /**
-    * Shuffle/scramble mutation operator for the permutation encoded genetic algorithm.
+    * Shuffle/scramble mutation operator for the permutation genetic algorithm. \n
     * Each individual is mutated with the set mutation probability.
     * In the mutated candidates, a random range of genes is selected and then shuffled.
     */
     class Shuffle final : public Mutation<size_t>
     {
     public:
-        using Mutation::Mutation;
+        /**
+        * Create a shuffle mutation operator.
+        *
+        * @param pm The mutation probability. Must be in the closed interval [0.0, 1.0].
+        * @param range_max The value of the range_max parameter. Must be in the closed interval [0.0, 1.0].
+        */
+        explicit Shuffle(double pm, double range_max = 0.5);
+
+        /**
+        * Set the value of the maximum range length parameter for the operator. \n
+        * The parameter determines the maximum length of the shuffled range of genes during mutation:
+        * the maximum length is @p rm * chrom_len. \n
+        * The value of this parameter must be in the closed interval [0.0, 1.0].
+        *
+        * @param rm The value of the range max parameter.
+        */
+        void range_max(double rm);
+
+        /** @ returns The current value of the range_max parameter. */
+        [[nodiscard]]
+        double range_max() const noexcept { return range_max_; }
+
     private:
-        void mutate(const GaInfo& ga, Candidate<size_t>& candidate) const override;
+        void mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const override;
+
+        double range_max_ = 0.5;
     };
 
     /**
-    * Shift/slide mutation operator for the permutation encoded genetic algorithm.
+    * Shift/slide mutation operator for the permutation encoded genetic algorithm. \n
     * Each individual is mutated with the set mutation proability.
     * In the mutated candidates, a random range of genes is selected and then moved to a
     * different position in the chromosome.
@@ -69,9 +115,32 @@ namespace genetic_algorithm::mutation::perm
     class Shift final : public Mutation<size_t>
     {
     public:
-        using Mutation::Mutation;
+        /**
+        * Create a shift mutation operator.
+        *
+        * @param pm The mutation probability. Must be in the closed interval [0.0, 1.0].
+        * @param range_max The value of the range_max parameter. Must be in the closed interval [0.0, 1.0].
+        */
+        explicit Shift(double pm, double range_max = 0.75);
+
+        /**
+        * Set the value of the maximum range length parameter for the operator. \n
+        * The parameter determines the maximum length of the shifted range of genes during mutation:
+        * the maximum length is @p rm * chrom_len. \n
+        * The value of this parameter must be in the closed interval [0.0, 1.0].
+        *
+        * @param rm The value of the range max parameter.
+        */
+        void range_max(double rm);
+
+        /** @ returns The current value of the range_max parameter. */
+        [[nodiscard]]
+        double range_max() const noexcept { return range_max_; }
+
     private:
-        void mutate(const GaInfo& ga, Candidate<size_t>& candidate) const override;
+        void mutate(const GaInfo& ga, Candidate<GeneType>& candidate) const override;
+
+        double range_max_ = 0.75;
     };
 
 } // namespace genetic_algorithm::mutation::perm
