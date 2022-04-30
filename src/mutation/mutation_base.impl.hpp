8f979453375e81324e8300f6d1b84bddd8472be8
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-04-30 11:08:03 +0200

    Update mutation_base.impl.hpp

--- src/mutation/mutation_base.impl.hpp
+++ src/mutation/mutation_base.impl.hpp
@@ -30,12 +30,15 @@ namespace genetic_algorithm::mutation
     template<Gene T>
     void Mutation<T>::operator()(const GaInfo& ga, Candidate<T>& candidate) const
     {
-        auto old_chrom = candidate.chromosome;
-        mutate(ga, candidate);
+        auto old_candidate = candidate;
 
-        if (candidate.chromosome != old_chrom)
+        mutate(ga, candidate);
+        candidate.is_evaluated = false;
+
+        if (candidate.chromosome == old_candidate.chromosome)
         {
-            candidate.is_evaluated = false;
+            candidate.fitness = std::move(old_candidate.fitness);
+            candidate.is_evaluated = old_candidate.is_evaluated;
         }
     }
 
