3285fd1fb0ab5eced4fd440342c614c87398b8d8
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-04-30 13:05:28 +0200

    Create lambda.hpp

---
+++ src/mutation/lambda.hpp
@@ -0,0 +1,55 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_MUTATION_LAMBDA_HPP
+#define GA_MUTATION_LAMBDA_HPP
+
+#include "mutation_base.decl.hpp"
+#include "../population/candidate.hpp"
+#include <functional>
+
+namespace genetic_algorithm::mutation::dtl
+{
+    template<Gene T>
+    class Lambda final : public Mutation<T>
+    {
+    public:
+        using MutationFunction = std::function<void(const GaInfo&, Candidate<T>&)>;
+
+        explicit Lambda(MutationFunction f);
+
+    private:
+        void mutate(const GaInfo& ga, Candidate<T>& candidate) const override;
+
+        MutationFunction mutate_;
+    };
+
+} // namespace genetic_algorithm::mutation::dtl
+
+
+/* IMPLEMENTATION */
+
+#include <utility>
+#include <stdexcept>
+#include <cassert>
+
+namespace genetic_algorithm::mutation::dtl
+{
+    template<Gene T>
+    Lambda<T>::Lambda(MutationFunction f)
+        : Mutation<T>(0.01)
+    {
+        if (!f) throw std::invalid_argument("The crossover function can't be a nullptr.");
+
+        mutate_ = std::move(f);
+    }
+
+    template<Gene T>
+    void Lambda<T>::mutate(const GaInfo& ga, Candidate<T>& candidate) const
+    {
+        assert(mutate_);
+        mutate_(ga, candidate);
+    }
+
+} // namespace genetic_algorithm::mutation::dtl
+
+#endif // !GA_MUTATION_LAMBDA_HPP
