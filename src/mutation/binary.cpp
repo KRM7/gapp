34672e66aabea0496e8ab50c2b1f6006e3a48a4e
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-05-14 14:34:55 +0200

    Update binary.cpp

--- src/mutation/binary.cpp
+++ src/mutation/binary.cpp
@@ -3,16 +3,17 @@
 #include "binary.hpp"
 #include "mutation_dtl.hpp"
 #include "../utility/rng.hpp"
+#include "../utility/utility.hpp"
 #include <cstddef>
 
 namespace genetic_algorithm::mutation::binary
 {
     void Flip::mutate(const GaInfo&, Candidate<GeneType>& candidate) const
     {
-        size_t flip_cnt = dtl::approxMutateCnt(candidate.chromosome.size(), pm_);
-        auto flipped_idxs = rng::sampleUnique(candidate.chromosome.size(), flip_cnt);
+        size_t flip_count = dtl::approxMutateCnt(candidate.chromosome.size(), pm_);
+        auto flipped_indices = rng::sampleUnique(0_sz, candidate.chromosome.size(), flip_count);
 
-        for (const auto& idx : flipped_idxs)
+        for (const auto& idx : flipped_indices)
         {
             candidate.chromosome[idx] = !bool(candidate.chromosome[idx]);
         }
