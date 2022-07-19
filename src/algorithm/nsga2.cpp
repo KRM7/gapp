200edea5a89694d3bba2b22b917726020f7780b7
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-19 22:12:01 +0200

    Create nsga2.cpp

---
+++ src/algorithm/nsga2.cpp
@@ -0,0 +1,8 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#include "nsga2.hpp"
+
+namespace genetic_algorithm::algorithm
+{
+
+} // namespace genetic_algorithm::algorithm
