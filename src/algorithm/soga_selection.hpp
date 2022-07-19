8dad2ad7f7b106b1c765a30872644d1e1f6fcb47
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-19 22:12:23 +0200

    Create soga_selection.hpp

---
+++ src/algorithm/soga_selection.hpp
@@ -0,0 +1,16 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_ALGORITHM_SOGA_SELECTION_HPP
+#define GA_ALGORITHM_SOGA_SELECTION_HPP
+
+namespace genetic_algorithm::selection
+{
+    class Roulette;
+    class Tournament;
+    class Rank;
+    class Sigma;
+    class Boltzmann;
+
+} // namespace genetic_algorithm::selection
+
+#endif // !GA_ALGORITHM_SOGA_SELECTION_HPP
