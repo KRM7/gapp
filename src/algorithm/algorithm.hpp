76ed22ca15933ae35ff9c504e26ae97ddd581907
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-19 22:15:07 +0200

    Create algorithm.hpp

---
+++ src/algorithm/algorithm.hpp
@@ -0,0 +1,13 @@
+/* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_ALGORITHM_ALGORITHM_HPP
+#define GA_ALGORITHM_ALGORITHM_HPP
+
+#include "algorithm_base.hpp"
+#include "single_objective.hpp"
+#include "soga_selection.hpp"
+#include "population_update.hpp"
+#include "nsga2.hpp"
+#include "nsga3.hpp"
+
+#endif // !GA_ALGORITHM_ALGORITHM_HPP
