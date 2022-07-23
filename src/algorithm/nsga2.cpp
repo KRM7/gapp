8cf3f0155a306fef269eb8986562c348af21e7a3
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-23 18:57:14 +0200

    moved nsga2 algorithm to separate files

--- src/algorithm/nsga2.cpp
+++ src/algorithm/nsga2.cpp
@@ -1,8 +1,179 @@
 /* Copyright (c) 2022 Krisztián Rugási. Subject to the MIT License. */
 
 #include "nsga2.hpp"
+#include "nd_sort.hpp"
+#include "../core/ga_info.hpp"
+#include "../population/population.hpp"
+#include "../utility/rng.hpp"
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <limits>
+#include <utility>
+#include <cstddef>
+#include <cassert>
 
 namespace genetic_algorithm::algorithm
 {
+    using detail::FitnessVector;
+    using detail::FitnessMatrix;
+    using dtl::ParetoFronts;
+
+    /* Calculate the crowding distances of the solutions in the NSGA2 algorithm. */ // TODO make this a private static member func
+    static std::vector<double> crowdingDistances(FitnessMatrix::const_iterator first, FitnessMatrix::const_iterator last, ParetoFronts pfronts)
+    {
+        assert(std::distance(first, last) >= 0);
+
+        std::vector<double> distances(size_t(last - first), 0.0);
+
+        using Iter = ParetoFronts::iterator;
+        using IterPair = std::pair<Iter, Iter>;
+
+        auto front_bounds = dtl::paretoFrontBounds(pfronts);
+
+        std::for_each(front_bounds.begin(), front_bounds.end(),
+        [&distances, fmat = first](const IterPair& bounds)
+        {
+            const auto& [first, last] = bounds;
+
+            /* Calculate distances along each fitness dimension */
+            for (size_t dim = 0; dim < fmat[0].size(); dim++)
+            {
+                std::sort(first, last,
+                [fmat, dim](const auto& lhs, const auto& rhs) noexcept
+                {
+                    return fmat[lhs.first][dim] < fmat[rhs.first][dim];
+                });
+
+                const auto& front = first->first;
+                const auto& back  = (last - 1)->first;
+
+                double finterval = fmat[back][dim] - fmat[front][dim];
+                finterval = std::max(finterval, 1E-6);
+
+                distances[front] = std::numeric_limits<double>::infinity();
+                distances[back]  = std::numeric_limits<double>::infinity();
+
+                for (auto it = std::next(first); it < std::prev(last); it++)
+                {
+                    size_t this_ = it->first;
+                    size_t next = std::next(it)->first;
+                    size_t prev = std::prev(it)->first;
+
+                    distances[this_] += (fmat[next][dim] - fmat[prev][dim]) / finterval;
+                }
+            }
+        });
+
+        return distances;
+    }
+
+    void NSGA2::initialize(const GaInfo& ga)
+    {
+        assert(ga.num_objectives() > 1);
+        assert(ga.population_size() != 0);
+
+        auto& fmat = ga.fitness_matrix();
+        auto pfronts = dtl::nonDominatedSort(fmat.begin(), fmat.end());
+
+        ranks_ = dtl::paretoRanks(pfronts);
+        dists_ = crowdingDistances(fmat.begin(), fmat.end(), std::move(pfronts));
+    }
+
+    bool NSGA2::crowdedCompare(size_t lidx, size_t ridx) const noexcept
+    {
+        return (ranks_[lidx] != ranks_[ridx]) ?
+            ranks_[lidx] < ranks_[ridx] :
+            dists_[lidx] > dists_[ridx];
+    }
+
+    size_t NSGA2::select(const GaInfo&, const FitnessMatrix& pop)
+    {
+        assert(!pop.empty() && pop.size() == ranks_.size());
+
+        size_t idx1 = rng::randomIdx(pop);
+        size_t idx2 = rng::randomIdx(pop);
+
+        return crowdedCompare(idx1, idx2) ? idx1 : idx2;
+    }
+
+    std::vector<size_t> NSGA2::nextPopulation(const GaInfo& ga,
+                                              FitnessMatrix::const_iterator parents_first,
+                                              FitnessMatrix::const_iterator children_first,
+                                              FitnessMatrix::const_iterator children_last)
+    {
+        assert(size_t(children_first - parents_first) == ga.population_size());
+        assert(size_t(children_last - parents_first) >= ga.population_size());
+        assert(std::all_of(parents_first, children_last, [&ga](const FitnessVector& f) { return f.size() == ga.num_objectives(); }));
+
+        std::vector<size_t> new_pop;
+        new_pop.reserve(ga.population_size());
+
+        auto pfronts = dtl::nonDominatedSort(parents_first, children_last);
+        ranks_ = dtl::paretoRanks(pfronts);
+        dists_ = crowdingDistances(parents_first, children_last, pfronts);
+
+        /* Keep track of the details of the candidates added to new_pop to avoid a second sort. */
+        std::vector<size_t> new_ranks;
+        std::vector<double> new_dists;
+        new_ranks.reserve(ga.population_size());
+        new_dists.reserve(ga.population_size());
+
+        /* Add entire fronts while possible. */
+        auto first = pfronts.begin();
+        auto last = dtl::nextFrontBegin(first, pfronts.end());
+        while (new_pop.size() + std::distance(first, last) <= ga.population_size())
+        {
+            for (; first != last; first++)
+            {
+                size_t idx = first->first;
+                new_pop.push_back(idx);
+                new_ranks.push_back(ranks_[idx]);
+                new_dists.push_back(dists_[idx]);
+            }
+            //first = last; // TODO
+            last = dtl::nextFrontBegin(first, pfronts.end());
+        }
+
+        /* Add the remaining candidates from the partial front if there is one. */
+        if (new_pop.size() != ga.population_size())
+        {
+            size_t remaining_indices = ga.population_size() - new_pop.size();
+
+            /* Keep track of the candidates added from the partial front,
+             * the crowding distances will only need to be updated for these candidates. */
+            dtl::ParetoFronts partial_front;
+            partial_front.reserve(remaining_indices);
+
+            std::sort(first, last,
+            [this](const std::pair<size_t, size_t>& lhs, const std::pair<size_t, size_t>& rhs) noexcept
+            {
+                size_t lidx = lhs.first;
+                size_t ridx = rhs.first;
+                return dists_[lidx] > dists_[ridx]; /* The ranks will always be the same */
+            });
+
+            for (; new_pop.size() != ga.population_size(); first++)
+            {
+                size_t idx = first->first;
+
+                new_pop.push_back(idx);
+                new_ranks.push_back(ranks_[idx]);
+                partial_front.push_back(*first);
+            }
+
+            auto changed_dists = crowdingDistances(parents_first, children_last, { partial_front });
+
+            for (size_t i = 0; i < remaining_indices; i++)
+            {
+                double new_dist = changed_dists[partial_front[i].first];
+                new_dists.push_back(new_dist);
+            }
+        }
+        ranks_ = std::move(new_ranks);
+        dists_ = std::move(new_dists);
+
+        return new_pop;
+    }
 
 } // namespace genetic_algorithm::algorithm
--- src/algorithm/nsga2.hpp
+++ src/algorithm/nsga2.hpp
@@ -7,8 +7,27 @@
 
 namespace genetic_algorithm::algorithm
 {
+    /**
+    * Non-dominated sorting genetic algorithm (NSGA-II) for multi-objective optimization.
+    */
+    class NSGA2 final : public Algorithm
+    {
+    public:
+        void initialize(const GaInfo& ga) override;
+        void prepareSelections(const GaInfo&, const FitnessMatrix&) override {}
+        size_t select(const GaInfo& ga, const FitnessMatrix& pop) override;
+        std::vector<size_t> nextPopulation(const GaInfo& ga,
+                                           FitnessMatrix::const_iterator first,
+                                           FitnessMatrix::const_iterator children_first,
+                                           FitnessMatrix::const_iterator last) override;
 
+    private:
+        std::vector<size_t> ranks_;
+        std::vector<double> dists_;
 
+        /* Returns true if Pop[lidx] is better than Pop[ridx]. */
+        bool crowdedCompare(size_t lidx, size_t ridx) const noexcept;
+    };
 
 } // namespace genetic_algorithm::algorithm
 
