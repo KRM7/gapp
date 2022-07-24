4520f5b063758a94acc81fbe35c2c0c531e7656f
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-24 21:29:58 +0200

    improved comments

--- src/algorithm/nd_sort.hpp
+++ src/algorithm/nd_sort.hpp
@@ -13,19 +13,22 @@ namespace genetic_algorithm::algorithm::dtl
     using detail::FitnessVector;
     using detail::FitnessMatrix;
 
-    /* Sorted (idx, rank) pairs. */
+    /* [solution idx, solution rank] pairs. */
     using ParetoFronts = std::vector<std::pair<size_t, size_t>>;
 
-    /* Non-dominated sorting for the multi-objective algorithms. Returns the pareto fronts (idx, rank pairs) of the population. */
+    /* Non-dominated sorting for the multi-objective algorithms.
+       Returns the pareto fronts (in the form of [idx, rank] pairs) of the population, in non-descending order based on the ranks. */
     ParetoFronts nonDominatedSort(FitnessMatrix::const_iterator first, FitnessMatrix::const_iterator last);
 
-    /* Returns the rank of each candidate based on the pareto fronts. */
+    /* Returns the pareto rank of each candidate based on the pareto fronts (best rank is 0). */
     std::vector<size_t> paretoRanks(const ParetoFronts& pareto_fronts);
 
-    /* Finds the first element of the front following the front which current is a part of. */
+    /* Finds the first element of the front following the front which @p current is a part of.
+       (Assumes that the range is sorted in non-descending order based on the ranks of the elements.) */
     ParetoFronts::iterator nextFrontBegin(ParetoFronts::iterator current, ParetoFronts::iterator last) noexcept;
 
-    /* Finds the first and last elements of each front in the pareto fronts vector. */
+    /* Finds the first and last elements of each front in the pareto fronts vector.
+       (Assumes that the range is sorted in non-descending order based on the ranks of the elements.) */
     auto paretoFrontBounds(ParetoFronts& pareto_fronts) -> std::vector<std::pair<ParetoFronts::iterator, ParetoFronts::iterator>>;
 
 } // namespace genetic_algorithm::algorithm::dtl
