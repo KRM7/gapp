e7b251ce333b6331a1cbe836385138190be2847a
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-24 21:19:24 +0200

    added description for the NSGA3 algorithm

--- src/algorithm/nsga3.hpp
+++ src/algorithm/nsga3.hpp
@@ -8,7 +8,25 @@
 namespace genetic_algorithm::algorithm
 {
     /**
-    * NSGA-III algorithm for many-objective optimization.
+    * NSGA-III algorithm, used for many-objective optimization.
+    * (This algorithm doesn't work for single-objective problems.) \n
+    * The aim of the algorithm is to find a set of solutions which are well-spread out
+    * along the entire pareto-front (in the objective-space). \n
+    * 
+    * The algorithm uses non-dominated sorting to sort the solutions into
+    * pareto fronts, and then selects the candidates of the best fronts for the population
+    * of the next generation. \n
+    * Candidates that belong to the same front are ranked using a predefined set of reference
+    * directions in the objective space. Candidates that are closest to reference directions
+    * which have less candidates associated with them, and candidates closer to reference directions
+    * are considered better. \n
+    * The reference directions are generated at the start of the run, and don't change throughout it. \n
+    * 
+    * The algorithm uses a selection operator that selects solutions based on these same criteria
+    * (their pareto ranks and their distances from the reference directions). \n
+    * 
+    * The selection and population update methods of this algorithm can't be changed. \n
+    * Has no parameters.
     */
     class NSGA3 final : public Algorithm
     {
@@ -23,6 +41,7 @@ namespace genetic_algorithm::algorithm
 
         using Point = std::vector<double>;
 
+        /* A reference point/direction with its associated niche-count. */
         struct RefPoint
         {
             const Point point;
@@ -32,7 +51,7 @@ namespace genetic_algorithm::algorithm
         };
 
     private:
-
+        /* Stats associated with each of the solutions. */
         struct CandidateInfo
         {
             size_t rank = 0;
@@ -40,6 +59,7 @@ namespace genetic_algorithm::algorithm
             double ref_dist = 0.0;
         };
 
+        /* Achievement scalarization function type. */
         using ASF = std::function<double(const std::vector<double>&)>;
 
         std::vector<CandidateInfo> sol_info_;
@@ -70,19 +90,19 @@ namespace genetic_algorithm::algorithm
         /* Find an approximation of the nadir point of the pareto front using the minimum of the extreme points. */
         static Point findNadirPoint(const std::vector<Point>& extreme_points);
 
-        /* TODO ... map fitness values onto unit simplex */
+        /* Map a fitness vector onto the unit simplex using the ideal and nadir points. */
         static FitnessVector normalize(const FitnessVector& fvec, const Point& ideal_point, const Point& nadir_point);
 
         /* Find the closest reference point to each candidate after normalization, and their distances. */
         void associatePopWithRefs(std::vector<CandidateInfo>& props, FitnessMatrix::const_iterator first, FitnessMatrix::const_iterator last, const std::vector<RefPoint>& refs);
 
-        /* Returns true if Pop[lhs] is better than Pop[rhs]. */
+        /* Return true if Pop[lhs] is better than Pop[rhs]. */
         bool nichedCompare(size_t lhs, size_t rhs) const noexcept;
 
         /* Return the niche counts of the ref points and assign niche counts to the candidates. */
         static void updateNicheCounts(std::vector<RefPoint>& refs, const std::vector<CandidateInfo>& props) noexcept;
 
-        /* Returns the niche counts of the given candidate. */
+        /* Return the niche counts of the given candidate. */
         size_t& nicheCountOf(const CandidateInfo& info) noexcept;
         size_t& nicheCountOf(size_t sol_idx) noexcept;
         const size_t& nicheCountOf(const CandidateInfo& info) const noexcept;
