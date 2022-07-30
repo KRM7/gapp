877bd2502cd75a6f6e0dc2b769e362296d162b9c
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-07-30 20:56:38 +0200

    display nsga2, results separately

--- test/mo_results/fplot.py
+++ test/mo_results/fplot.py
@@ -1,25 +1,27 @@
 # Script for plotting the fitness values of the population of the NSGA-II algorithm.
 
 import matplotlib.pyplot as plt
+import os
 
 # Filenames
-f11 = "nsga2_kur_sols.txt"
-f12 = "nsga3_kur_sols.txt"
+f11 = "NSGA2_KUR_sols.txt"
+f12 = "NSGA3_KUR_sols.txt"
 
-f21 = "nsga2_zdt2_sols.txt"
-f22 = "nsga3_zdt2_sols.txt"
+f21 = "NSGA2_ZDT2_sols.txt"
+f22 = "NSGA3_ZDT2_sols.txt"
 
-f31 = "nsga2_zdt3_sols.txt"
-f32 = "nsga3_zdt3_sols.txt"
+f31 = "NSGA2_ZDT3_sols.txt"
+f32 = "NSGA3_ZDT3_sols.txt"
 
-f41 = "nsga2_zdt6_sols.txt"
-f42 = "nsga3_zdt6_sols.txt"
+f41 = "NSGA2_ZDT6_sols.txt"
+f42 = "NSGA3_ZDT6_sols.txt"
 
-f51 = "nsga2_dtlz1_sols.txt"
-f52 = "nsga3_dtlz1_sols.txt"
+f51 = "NSGA2_DTLZ1_sols.txt"
+f52 = "NSGA3_DTLZ1_sols.txt"
 
-f61 = "nsga2_dtlz2_sols.txt"
-f62 = "nsga3_dtlz2_sols.txt"
+f61 = "NSGA2_DTLZ2_sols.txt"
+f62 = "NSGA3_DTLZ2_sols.txt"
+
 
 # Read and return data points from file
 def readData(name):
@@ -27,38 +29,56 @@ def readData(name):
         lines = f.readlines()
         x = [float(line.split()[0]) for line in lines]
         y = [float(line.split()[1]) for line in lines]
+
     return x, y
+
+
+# Read and return data points from file
+def readData3D(name):
+    with open(name) as f:
+        lines = f.readlines()
+        x = [float(line.split()[0]) for line in lines]
+        y = [float(line.split()[1]) for line in lines]
+        z = [float(line.split()[2]) for line in lines]
+
+    return x, y, z
+
 
 # Plot the fitness values of the NSGA-II and III for the same problem
 def plotData(name1, name2, title):
+    if not os.path.isfile(name1) or not os.path.isfile(name2):
+        return
+
     x1, y1 = readData(name1)
     x2, y2 = readData(name2)
 
-    fig, ax = plt.subplots()
-    ax.scatter(x1, y1, c = "red", marker = "x", label = "NSGA-II", linewidths = 1.0)
-    ax.scatter(x2, y2, c = "blue", marker = "x", label = "NSGA-III", linewidths = 1.0)
-    ax.set(xlabel = "f1", ylabel = "f2")
-    ax.set_title(title)
-    ax.invert_xaxis()
-    ax.invert_yaxis()
-    fig.legend(loc = "upper right")
+    fig, ax = plt.subplots(1,3)
+    ax[0].scatter(x1, y1, c = "red", marker = ".", label = "NSGA-II", linewidths = 1.0)
+    ax[0].scatter(x2, y2, c = "blue", marker = ".", label = "NSGA-III", linewidths = 1.0)
 
+    ax[1].scatter(x1, y1, c = "red", marker  = ".", label = "NSGA-II", linewidths = 1.0)
+
+    ax[2].scatter(x2, y2, c = "blue", marker = ".", label = "NSGA-III", linewidths = 1.0)
+
+    for axis in ax:
+        axis.set(xlabel = "f1", ylabel = "f2")
+        axis.set_title(title)
+        axis.invert_xaxis()
+        axis.invert_yaxis()
+        axis.set_aspect("equal")
+
+    fig.legend(loc = "upper right")
+    fig.set_tight_layout(True)
     plt.show()
-    return
+
 
 # Plot the fitness values of the last population
 def plotData3D(name1, name2, title):
-    with open(name1) as f:
-        lines = f.readlines()
-        x1 = [float(line.split()[0]) for line in lines]
-        y1 = [float(line.split()[1]) for line in lines]
-        z1 = [float(line.split()[2]) for line in lines]
+    if not os.path.isfile(name1) or not os.path.isfile(name2):
+        return
 
-    with open(name2) as f:
-        lines = f.readlines()
-        x2 = [float(line.split()[0]) for line in lines]
-        y2 = [float(line.split()[1]) for line in lines]
-        z2 = [float(line.split()[2]) for line in lines]
+    x1, y1, z1 = readData3D(name1)
+    x2, y2, z2 = readData3D(name2)
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 2, 1, projection="3d")
@@ -78,9 +98,9 @@ def plotData3D(name1, name2, title):
     ax.invert_zaxis()
 
     fig.legend(loc = "upper right")
-
+    fig.set_tight_layout(True)
     plt.show()
-    return
+
 
 def main():
     plotData(f11, f12, "KUR fitness values")
@@ -90,6 +110,7 @@ def main():
 
     plotData3D(f51, f52, "DTLZ1")
     plotData3D(f61, f62, "DTLZ2")
+
 
 if __name__ == "__main__":
     main()
