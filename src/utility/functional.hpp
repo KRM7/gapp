13aada4ab70229bdf411078404a408e1880154d0
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-08-25 21:23:29 +0200

    add utility functions

--- src/utility/functional.hpp
+++ src/utility/functional.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <vector>
+#include <cmath>
 #include <utility>
 #include <limits>
 #include <type_traits>
@@ -28,7 +29,8 @@ namespace genetic_algorithm::detail
     constexpr auto compose(F&& f) noexcept
     {
         return [f = lforward<F>(f)] <typename... Args>
-        (Args&&... args) requires std::invocable<F, Args...>
+        (Args&&... args) noexcept(std::is_nothrow_invocable_v<F, Args...>)
+        requires std::invocable<F, Args...>
         {
             return std::invoke(f, std::forward<Args>(args)...);
         };
@@ -124,6 +126,86 @@ namespace genetic_algorithm::detail
         return flat;
     }
 
+    template<typename T>
+    constexpr auto multiply_by(const T& multiplier)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& value) noexcept(noexcept(value * value))
+        {
+            return multiplier * value;
+        };
+    }
+
+    template<typename T>
+    constexpr auto divide_by(const T& divisor)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [idiv = 1.0 / divisor](const T& value) noexcept(noexcept(value * value))
+        {
+            return idiv * value;
+        };
+    }
+
+    template<typename T>
+    constexpr auto add(const T& increment)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& value) noexcept(noexcept(value + value))
+        {
+            return value + increment;
+        };
+    }
+
+    template<typename T>
+    constexpr auto subtract(const T& decrement)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& value) noexcept(noexcept(value - value))
+        {
+            return value - decrement;
+        };
+    }
+
+    template<typename T>
+    constexpr auto multiply_add(const T& multiplier, const T& increment)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& value) noexcept(noexcept(value * value - value))
+        {
+            return multiplier * value + increment;
+        };
+    }
+
+    template<typename T>
+    requires (std::is_arithmetic_v<T>)
+    constexpr auto multiply_add(const T& multiplier, const T& increment) noexcept
+    {
+        return [=](const T& value) noexcept
+        {
+            return std::fma(multiplier, value, increment);
+        };
+    }
+
+    template<typename T>
+    constexpr auto equal_to(const T& rhs)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& lhs) noexcept(noexcept(lhs == lhs))
+        {
+            return lhs == rhs;
+        };
+    }
+
+    template<typename T>
+    constexpr auto not_equal_to(const T& rhs)
+    noexcept(std::is_nothrow_copy_constructible_v<T>)
+    {
+        return [=](const T& lhs) noexcept(noexcept(lhs != lhs))
+        {
+            return lhs != rhs;
+        };
+    }
+    
 } // namespace genetic_algorithm::detail
 
 #endif // !GA_FUNCTIONAL_HPP
