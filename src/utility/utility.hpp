bf56704fa26bd8d6b8143d06c5b1035a90fe0199
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-08-27 12:42:12 +0200

    added GA_THROW, GA_ASSERT

--- src/utility/utility.hpp
+++ src/utility/utility.hpp
@@ -5,11 +5,14 @@
 
 #include <limits>
 #include <execution>
+#include <cassert>
 #include <cstddef>
+
 
 #ifndef GA_EPSILON
 #define GA_EPSILON (4 * std::numeric_limits<double>::epsilon())
 #endif
+
 
 #ifndef GA_EXCUTION_UNSEQ
 #define GA_EXECUTION_UNSEQ std::execution::par_unseq
@@ -18,11 +21,18 @@
 #define GA_EXECUTION_SEQ std::execution::par
 #endif
 
+
+#define GA_ASSERT(condition, msg) assert((condition) && msg)
+#define GA_THROW(exception_type, msg) throw exception_type(msg)
+
+
 #ifndef GA_SEED
 #define GA_SEED 0x3da99432ab975d26LL
 #endif
 
+
 #define GA_UNUSED(...) (void)(sizeof(__VA_ARGS__))
+
 
 namespace genetic_algorithm
 {
