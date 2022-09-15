d2ac95f0616c40a92da38d233dfb220d747c568f
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-09-15 21:55:35 +0200

    add iterator_interface

--- src/utility/iterators.hpp
+++ src/utility/iterators.hpp
@@ -7,17 +7,40 @@
 
 namespace genetic_algorithm::detail
 {
+    /*
+    * The following should be implemented in Derived:
+    *   begin(), end() with const overloads
+    */
     template<typename Derived>
-    class reverse_iterator_interface
+    class iterator_interface
     {
     public:
-        /* Requires begin(), end(), cbegin(), cend() to be implemented in Derived. */
-        auto rbegin() noexcept        { return std::make_reverse_iterator(derived().end()); }
-        auto rbegin() const noexcept  { return std::make_reverse_iterator(derived().end()); }
-        auto crbegin() const noexcept { return std::make_reverse_iterator(derived().cend()); }
-        auto rend() noexcept          { return std::make_reverse_iterator(derived().begin()); }
-        auto rend() const noexcept    { return std::make_reverse_iterator(derived().begin()); }
-        auto crend() const noexcept   { return std::make_reverse_iterator(derived().cbegin()); }
+        auto cbegin() const { return derived.begin(); };
+        auto cend() const { return derived.end(); };
+    private:
+        constexpr Derived& derived() noexcept { return static_cast<Derived&>(this); }
+        constexpr const Derived& derived() const noexcept { return static_cast<const Derived&>(this); }
+    };
+
+    /*
+    * The following should be implemented in Derived:
+    *   begin(), end() with const overloads
+    */
+    template<typename Derived>
+    class reverse_iterator_interface : public iterator_interface<Derived>
+    {
+    public:
+        auto rbegin()        { return std::make_reverse_iterator(derived().end()); }
+        auto rbegin() const  { return std::make_reverse_iterator(derived().end()); }
+        auto crbegin() const { return std::make_reverse_iterator(derived().end()); }
+        auto rend()          { return std::make_reverse_iterator(derived().begin()); }
+        auto rend() const    { return std::make_reverse_iterator(derived().begin()); }
+        auto crend() const   { return std::make_reverse_iterator(derived().begin()); }
+
+    private:
+        constexpr Derived& derived() noexcept { return static_cast<Derived&>(this); }
+        constexpr const Derived& derived() const noexcept { return static_cast<const Derived&>(this); }
+    };
 
     private:
         constexpr Derived& derived() noexcept { return *static_cast<Derived*>(this); }
