3946f5ade59c8fc0711bea909b5ecf94ae4f30aa
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-09-27 00:08:31 +0200

    add quasirandom simplex point gen function

--- src/algorithm/reference_lines.cpp
+++ src/algorithm/reference_lines.cpp
@@ -4,6 +4,7 @@
 #include "../utility/algorithm.hpp"
 #include "../utility/functional.hpp"
 #include "../utility/rng.hpp"
+#include "../utility/qrng.hpp"
 #include "../utility/math.hpp"
 #include "../utility/utility.hpp"
 #include <algorithm>
@@ -40,6 +41,48 @@ namespace genetic_algorithm::algorithm::dtl
         while (n--) reference_points.push_back(randomSimplexPoint(dim));
 
         return reference_points;
+    }
+
+    /* Generate n quasirandom points on a unit simplex in dim dimensions. */
+    static std::vector<Point> quasirandomSimplexPoints(size_t dim, size_t n)
+    {
+        assert(dim > 0);
+
+        detail::QuasiRandom qrng(dim - 1);
+
+        std::vector<Point> points;
+
+        for (size_t p = 0; p < n; p++)
+        {
+            Point point(dim + 1, 0.0);
+            Point qrand = qrng();
+            std::copy(qrand.begin(), qrand.end(), point.begin() + 1);
+            point.back() = 1.0;
+
+            for (size_t i = dim - 1; i > 1; i--)
+            {
+                for (size_t j = 1; j < i; j++)
+                {
+                    if (point[i] < point[j])
+                    {
+                        for (size_t k = j; k < i + 1; k++)
+                        {
+                            point[k] = point[j - 1] + point[i + 1] - point[k];
+                        }
+                    }
+                }
+            }
+
+            for (size_t i = 0; i < dim; i++)
+            {
+                point[i] = point[i + 1] - point[i];
+            }
+            point.pop_back();
+
+            points.push_back(point);
+        }
+
+        return points;
     }
 
     /* Generate reference points by picking n points from a set of randomly generated points. */
@@ -84,7 +127,8 @@ namespace genetic_algorithm::algorithm::dtl
 
     std::vector<Point> generateReferencePoints(size_t dim, size_t n)
     {
-        std::vector<Point> points = generateRandomRefpointsPick(dim, n);
+        //std::vector<Point> points = generateRandomRefpointsPick(dim, n);
+        std::vector<Point> points = quasirandomSimplexPoints(dim, n);
         std::transform(points.begin(), points.end(), points.begin(), [](Point p) noexcept { return math::normalizeVector(std::move(p)); });
 
         return points;
