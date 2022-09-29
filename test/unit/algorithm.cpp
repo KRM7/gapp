a61043c4341321d06964e29a0883b5ec4298738b
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-09-29 20:48:45 +0200

    Update algorithm.cpp

--- test/unit/algorithm.cpp
+++ test/unit/algorithm.cpp
@@ -1,7 +1,65 @@
-#include "utility/algorithm.hpp"
 #include <catch2/catch_test_macros.hpp>
+#include "utility/algorithm.hpp"
 
-TEST_CASE( "Testing", "[simple]")
+using namespace genetic_algorithm;
+
+TEST_CASE("index_vector", "[algorithm]")
 {
-    REQUIRE( 1 == 1);
+    auto indices = detail::index_vector(3);
+
+    REQUIRE(indices.size() == 3);
+    REQUIRE(indices[0] == 0);
+    REQUIRE(indices[2] == 2);
+
+    indices = detail::index_vector(4, 1);
+
+    REQUIRE(indices.size() == 4);
+    REQUIRE(indices[0] == 1);
+    REQUIRE(indices[3] == 4);
+}
+
+TEST_CASE("argsort", "[algorithm]")
+{
+    const std::vector nums = { 4.0, 0.0, 2.0, 1.0 };
+
+    SECTION("simple ascending")
+    {
+        const auto indices = detail::argsort(nums.begin(), nums.end());
+
+        REQUIRE(indices ==  std::vector<size_t>{ 1, 3, 2, 0 });
+    }
+
+    SECTION("descending with reverse_iterators")
+    {
+        const auto indices = detail::argsort(nums.rbegin(), nums.rend());
+
+        REQUIRE(indices ==  std::vector<size_t>{ 0, 2, 3, 1 });
+    }
+
+    SECTION("descending with comparison function")
+    {
+        const auto indices = detail::argsort(nums.begin(), nums.end(), std::greater<>{});
+
+        REQUIRE(indices ==  std::vector<size_t>{ 0, 2, 3, 1 });
+    }
+
+    SECTION("empty range")
+    {
+        const auto indices = detail::argsort(nums.begin(), nums.begin());
+
+        REQUIRE(indices.empty());
+    }
+
+    SECTION("invalid range")
+    {
+        decltype(nums) another_range;
+
+        REQUIRE_THROWS(detail::argsort(nums.end(), nums.begin()));
+        REQUIRE_THROWS(detail::argsort(nums.begin(), another_range.begin()));
+    }
+}
+
+TEST_CASE("partial_argsort", "[algorithm]")
+{
+    
 }
