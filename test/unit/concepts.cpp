ce67cb01c4b715c5e6039578849bcb0dad6f55a7
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-10-02 20:45:24 +0200

    add concept tests

---
+++ test/unit/concepts.cpp
@@ -0,0 +1,61 @@
+#include <catch2/catch_test_macros.hpp>
+#include "utility/concepts.hpp"
+#include <vector>
+#include <array>
+#include <set>
+#include <string>
+
+using namespace genetic_algorithm;
+
+template<typename T>
+struct A {};
+
+struct B : A<int> {};
+
+TEST_CASE("hashable", "[concepts]")
+{
+    STATIC_REQUIRE(detail::Hashable<int>);
+    STATIC_REQUIRE(detail::Hashable<std::string>);
+    STATIC_REQUIRE(!detail::Hashable<A<double>>);
+    STATIC_REQUIRE(!detail::Hashable<std::vector<int>>);
+}
+
+TEST_CASE("specialization_of", "[concepts]")
+{
+    STATIC_REQUIRE(detail::SpecializationOf<std::vector<int>, std::vector>);
+    STATIC_REQUIRE(detail::SpecializationOf<A<double>, A>);
+
+    STATIC_REQUIRE(!detail::SpecializationOf<void, std::vector>);
+    STATIC_REQUIRE(!detail::SpecializationOf<int, std::vector>);
+}
+
+TEST_CASE("derived_from_specialization_of", "[concepts]")
+{
+    STATIC_REQUIRE(detail::DerivedFromSpecializationOf<B, A>);
+    STATIC_REQUIRE(detail::DerivedFromSpecializationOf<A<long>, A>);
+
+    STATIC_REQUIRE(!detail::DerivedFromSpecializationOf<void, A>);
+    STATIC_REQUIRE(!detail::DerivedFromSpecializationOf<int, std::vector>);
+}
+
+TEST_CASE("container", "[concepts]")
+{
+    STATIC_REQUIRE(detail::Container<std::vector<int>>);
+    STATIC_REQUIRE(detail::Container<std::array<int, 3>>);
+    STATIC_REQUIRE(detail::Container<std::set<std::string>>);
+
+    STATIC_REQUIRE(!detail::Container<std::vector<bool>>);
+    STATIC_REQUIRE(!detail::Container<int*>);
+    STATIC_REQUIRE(!detail::Container<void>);
+}
+
+TEST_CASE("indexable_container", "[concepts]")
+{
+    STATIC_REQUIRE(detail::IndexableContainer<std::vector<int>>);
+    STATIC_REQUIRE(detail::IndexableContainer<std::array<int, 3>>);
+    STATIC_REQUIRE(detail::IndexableContainer<std::string>);
+
+    STATIC_REQUIRE(!detail::IndexableContainer<std::vector<bool>>);
+    STATIC_REQUIRE(!detail::IndexableContainer<int*>);
+    STATIC_REQUIRE(!detail::IndexableContainer<void>);
+}
