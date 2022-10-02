b08fec003dc870dae347be1c8f092009faf4a545
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-10-02 21:23:47 +0200

    update catch install script

--- build/install_catch.sh
+++ build/install_catch.sh
@@ -4,5 +4,5 @@ git clone https://github.com/catchorg/Catch2.git
 cd Catch2
 git checkout tags/v3.1.0
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
-sudo env "PATH=$PATH" cmake --build build/ --target install
+cmake --build build/ --target install
 cd ..
