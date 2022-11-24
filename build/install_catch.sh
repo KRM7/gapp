6505cbf0eefd800f73e90c7acd3f4e082b4ef2ab
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-11-24 23:22:02 +0100

    fix workflows

--- build/install_catch.sh
+++ build/install_catch.sh
@@ -6,4 +6,5 @@ git checkout tags/v3.1.0
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
 cmake --build build/ --target install --config Debug
 cmake --build build/ --target install --config Release
+cmake --build build/ --target install --config RelWithDebInfo
 cd ..
