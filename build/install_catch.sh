c12b24604019b6fee894ba84fd2623fdd1f27168
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-11-28 23:32:54 +0100

    Update install_catch.sh

--- build/install_catch.sh
+++ build/install_catch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-  
+
 git clone https://github.com/catchorg/Catch2.git
 cd Catch2
 git checkout tags/v3.1.0
-cmake -Bbuild -H. -DBUILD_TESTING=OFF
+cmake -Bbuild -S. -DBUILD_TESTING=OFF
 cmake --build build/ --target install --config Debug
 cmake --build build/ --target install --config Release
 cmake --build build/ --target install --config RelWithDebInfo
