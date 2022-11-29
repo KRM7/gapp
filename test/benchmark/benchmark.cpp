686d055b2d2e459da6655adc643884eb79154e79
KRM7 <70973547+KRM7@users.noreply.github.com>
2022-11-29 20:16:26 +0100

    Update benchmark.cpp

--- test/benchmark/benchmark.cpp
+++ test/benchmark/benchmark.cpp
@@ -9,9 +9,9 @@
 
 int main()
 {
-    begin:
-    //binary_rastrigin();
-    /*binary_rosenbrock();
+    //begin:
+    binary_rastrigin();
+    binary_rosenbrock();
     binary_schwefel();
     binary_griewank();
     binary_ackley();
@@ -20,33 +20,33 @@ int main()
     real_rosenbrock();
     real_schwefel();
     real_griewank();
-    real_ackley();*/
-    //begin:
-    //perm_tsp52();
-    //perm_tsp76();
-    //perm_tsp124();
-        //perm_tsp152();
+    real_ackley();
+
+    perm_tsp52();
+    perm_tsp76();
+    perm_tsp124();
+        perm_tsp152();
     perm_tsp226();
-    //perm_tsp299();
-    //perm_tsp439();
+    perm_tsp299();
+    perm_tsp439();
 
-    //integer_hello();
-    //integer_sentence();
+    integer_hello();
+    integer_sentence();
 
-    //nsga2_kur();
-    //nsga2_zdt2();
-    //nsga2_zdt3();
-    //nsga2_zdt6();
+    nsga2_kur();
+    nsga2_zdt2();
+    nsga2_zdt3();
+    nsga2_zdt6();
 
-    //nsga3_kur();
-    //nsga3_zdt2();
-    //nsga3_zdt3();
-    //nsga3_zdt6();
-    //
-    //nsga2_dtlz1();
-    //nsga2_dtlz2();
-    //begin:
-    //nsga3_dtlz1();
-    //nsga3_dtlz2();
-    goto begin;
+    nsga3_kur();
+    nsga3_zdt2();
+    nsga3_zdt3();
+    nsga3_zdt6();
+    
+    nsga2_dtlz1();
+    nsga2_dtlz2();
+
+    nsga3_dtlz1();
+    nsga3_dtlz2();
+    //goto begin;
 }
