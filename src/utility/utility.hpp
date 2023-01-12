7fc4c6494ce2da1c83356fc615fca5e3b997c0f7
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-01-12 23:25:23 +0100

    add pure, const, nonnull annotation macros

--- src/utility/utility.hpp
+++ src/utility/utility.hpp
@@ -21,6 +21,29 @@
 #endif
 
 
+#if defined(__GNUC__) || defined(__clang__)
+#   define GA_CONST __attribute__((const))
+#elif defined(_MSC_VER)
+#   define GA_CONST __declspec(noalias)
+#else
+#   define GA_CONST
+#endif
+
+
+#if defined(__GNUC__) || defined(__clang__)
+#   define GA_PURE __attribute__((pure))
+#else
+#   define GA_PURE
+#endif
+
+
+#if defined(__GNUC__) || defined(__clang__)
+#   define GA_NON_NULL __attribute__((nonnull))
+#else
+#   define GA_NON_NULL
+#endif
+
+
 #ifndef GA_EXCUTION_UNSEQ
 #   define GA_EXECUTION_UNSEQ std::execution::par_unseq
 #endif
