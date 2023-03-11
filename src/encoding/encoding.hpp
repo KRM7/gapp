d5564d244c21f0b905e72dcbbe7b4010112a268c
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-03-11 23:34:45 +0100

    Create encoding.hpp

---
+++ src/encoding/encoding.hpp
@@ -0,0 +1,12 @@
+﻿/* Copyright (c) 2023 Krisztián Rugási. Subject to the MIT License. */
+
+#ifndef GA_ENCODING_ENCODING_HPP
+#define GA_ENCODING_ENCODING_HPP
+
+#include "gene_types.hpp"
+#include "binary.hpp"
+#include "real.hpp"
+#include "permutation.hpp"
+#include "integer.hpp"
+
+#endif // !GA_ENCODING_ENCODING_HPP
