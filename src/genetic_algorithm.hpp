5cc23b52ffbb467d7c6b718f3419bacd9ad6b3f3
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-03-21 00:12:43 +0100

    Update genetic_algorithm.hpp

--- src/genetic_algorithm.hpp
+++ src/genetic_algorithm.hpp
@@ -4,16 +4,17 @@
 #define GA_GENETIC_ALGORITHM_HPP
 
 #include "utility/rng.hpp"
+#include "population/candidate.hpp"
+#include "population/population.hpp"
 #include "algorithm/algorithm.hpp"
 #include "crossover/crossover.hpp"
 #include "mutation/mutation.hpp"
 #include "stop_condition/stop_condition.hpp"
 #include "stop_condition/composite.hpp"
+#include "core/fitness_function.hpp"
+#include "core/ga_info.hpp"
 #include "core/ga_base.hpp"
-#include "encoding/binary.hpp"
-#include "encoding/real.hpp"
-#include "encoding/permutation.hpp"
-#include "encoding/integer.hpp"
+#include "encoding/encoding.hpp"
 #include "problems/problems.hpp"
 
 namespace ga = genetic_algorithm;
