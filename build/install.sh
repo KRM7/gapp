6fe4bf3ba589bc499f6119e3104217df348cec82
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-05-21 12:21:21 +0200

    Update install.sh

--- build/install.sh
+++ build/install.sh
@@ -9,15 +9,15 @@ echo -e "\nThe build directory is ${BUILD_DIR}.\n"
 
 echo -e "Installing Debug configuration.\n"
 cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF "$@"
-cmake --build $BUILD_DIR --config Debug
+cmake --build $BUILD_DIR --config Debug --parallel
 cmake --install $BUILD_DIR --config Debug
 
 echo -e "Installing RelWithDebInfo configuration.\n"
 cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=OFF "$@"
-cmake --build $BUILD_DIR --config RelWithDebInfo
+cmake --build $BUILD_DIR --config RelWithDebInfo --parallel
 cmake --install $BUILD_DIR --config RelWithDebInfo
 
 echo -e "Installing Release configuration.\n"
 cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF "$@"
-cmake --build $BUILD_DIR --config Release
+cmake --build $BUILD_DIR --config Release --parallel
 cmake --install $BUILD_DIR --config Release
