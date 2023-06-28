e6c894cbd24b7c11c4013a0bedfda7d1c91bc8c5
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-06-28 22:07:16 +0200

    add basic examples

---
+++ examples/2_basic_single_objective.cpp
@@ -0,0 +1,26 @@
+/* Example showing a more detailed usage of a real-encoded, single-objective algorithm. */
+
+#include "gapp.hpp" // include everything
+#include <iostream>
+#include <cmath>
+
+using namespace gapp;
+
+class SinX : public FitnessFunction<RealGene, 1>
+{
+    FitnessVector invoke(const Chromosome<RealGene>& x) const override { return { std::sin(x[0]) }; }
+};
+
+int main()
+{
+    RCGA GA{ /* population_size = */ 100 };
+
+    GA.algorithm(algorithm::SingleObjective{ selection::Tournament{}, replacement::KeepBest{} });
+    GA.crossover_method(crossover::real::Wright{ 0.8_p });
+    GA.mutation_method(mutation::real::Gauss{ 0.1_p });
+    GA.stop_condition(stopping::FitnessBestStall{ 5 });
+
+    auto solutions = GA.solve(SinX{}, Bounds{ 0.0, 3.14 });
+
+    std::cout << "The maximum of sin(x) in [0.0, 3.14] is at x = " << solutions[0].chromosome[0];
+}
---
+++ examples/3_basic_multi_objective.cpp
@@ -0,0 +1,38 @@
+/* Example showing a more detailed usage of a real-encoded, multi-objective algorithm. */
+
+#include "gapp.hpp" // include everything
+#include <iostream>
+#include <cmath>
+
+using namespace gapp;
+
+class Kursawe2 : public FitnessFunction<RealGene, 2>
+{
+    FitnessVector invoke(const Chromosome<RealGene>& x) const override
+    {
+        const double f1 = 10.0 * std::exp(-0.2 * std::sqrt(std::pow(x[0], 2) + std::pow(x[1], 2)));
+        const double f21 = std::pow(std::abs(x[0]), 0.8) + 5.0 * std::sin(std::pow(x[0], 3));
+        const double f22 = std::pow(std::abs(x[1]), 0.8) + 5.0 * std::sin(std::pow(x[1], 3));
+
+        return { -f1, -f21 - f22 };
+    }
+};
+
+int main()
+{
+    RCGA GA{ /* population_size = */ 20 };
+
+    GA.algorithm(algorithm::NSGA3{});
+    GA.crossover_method(crossover::real::BLXa{});
+    GA.mutation_method(mutation::real::Boundary{ 0.1 });
+    GA.stop_condition(stopping::FitnessMeanStall{ 5 });
+
+    auto solutions = GA.solve(Kursawe2{}, Bounds{ 0.0, 3.14 });
+
+    std::cout << "The optimal solutions of the Kursawe function in 2 dimensions are:\n";
+    std::cout << "x1\t|\tx2\n";
+    for (const Candidate<RealGene>& sol : solutions)
+    {
+        std::cout << sol.chromosome[0] << "\t|\t" << sol.chromosome[1] << "\n";
+    }
+}
