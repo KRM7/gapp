d03bb718a6eead6950a950d5430c71f278ec81d0
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-08-08 21:53:38 +0200

    restore old GA state after the runs

---
+++ test/unit/scope_exit.cpp
@@ -0,0 +1,43 @@
+﻿/* Copyright (c) 2023 Krisztián Rugási. Subject to the MIT License. */
+
+#include <catch2/catch_test_macros.hpp>
+#include "utility/scope_exit.hpp"
+
+using namespace gapp::detail;
+
+
+TEST_CASE("scope_exit", "[utility]")
+{
+    int n = 0;
+
+    {
+        ScopeExit on_exit{ [&] { n = 2; } };
+        REQUIRE(n == 0);
+    }
+    REQUIRE(n == 2);
+
+    {
+        ScopeExit on_exit{ [&] { n = 3; } };
+        on_exit.release();
+    }   
+    REQUIRE(n == 2);
+}
+
+TEST_CASE("restore_on_exit", "[utility]")
+{
+    int n = 3;
+    double f = 2.5;
+
+    {
+        RestoreOnExit on_exit(n, f);
+
+        n = 10;
+        f = 0.2;
+
+        REQUIRE(n == 10);
+        REQUIRE(f == 0.2);
+    }
+
+    REQUIRE(n == 3);
+    REQUIRE(f == 2.5);
+}
