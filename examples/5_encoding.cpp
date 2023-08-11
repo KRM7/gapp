796a24276169caa3d6386dbf5dda147719573169
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-08-11 21:10:44 +0200

    Create 5_encoding.cpp

---
+++ examples/5_encoding.cpp
@@ -0,0 +1,32 @@
+﻿/* Example showing the usage of the GAs with different encodings. */
+
+#include "gapp.hpp"
+
+using namespace gapp;
+
+int main()
+{
+    // binary encoding
+    {
+        BinaryGA ga;
+        ga.solve(problems::Sphere{ 3 });
+    }
+
+    // real encoding
+    {
+        RCGA ga;
+        ga.solve(problems::Sphere{ 3 }, Bounds{ -10.0, 10.0 });
+    }
+
+    // permutation encoding
+    {
+        PermutationGA ga;
+        ga.solve(problems::TSP52{});
+    }
+
+    // integer encoding
+    {
+        IntegerGA ga;
+        ga.solve(problems::StringFinder{ "Hello" }, Bounds<IntegerGene>{ 'A', 'z' });
+    }
+}
