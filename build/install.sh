4ea28e56b0229e55e890b1f83423df27170cbc01
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-08-18 18:41:30 +0200

    minor build system improvements

--- build/install.sh
+++ build/install.sh
@@ -8,16 +8,16 @@ BUILD_DIR=$(dirname $(realpath "$0"))
 echo -e "\nThe build directory is ${BUILD_DIR}.\n"
 
 echo -e "Installing Debug configuration.\n"
-cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF "$@"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF "$@"
 cmake --build $BUILD_DIR --config Debug --parallel
 cmake --install $BUILD_DIR --config Debug
 
 echo -e "Installing RelWithDebInfo configuration.\n"
-cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=OFF "$@"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=OFF "$@"
 cmake --build $BUILD_DIR --config RelWithDebInfo --parallel
 cmake --install $BUILD_DIR --config RelWithDebInfo
 
 echo -e "Installing Release configuration.\n"
-cmake $BUILD_DIR/.. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF "$@"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF "$@"
 cmake --build $BUILD_DIR --config Release --parallel
 cmake --install $BUILD_DIR --config Release
--- build/install_catch.sh
+++ build/install_catch.sh
@@ -1,10 +1,26 @@
 ﻿#!/bin/sh
 
-git clone https://github.com/catchorg/Catch2.git
-cd Catch2
-git checkout tags/v3.3.0
-cmake -Bbuild -S. -DBUILD_TESTING=OFF
-cmake --build build/ --target install --config Debug
-cmake --build build/ --target install --config Release
-cmake --build build/ --target install --config RelWithDebInfo
-cd ..
+echo -e "Installing Catch2...\n"
+
+git clone -b v3.3.0 https://github.com/catchorg/Catch2.git
+mkdir Catch2/build
+
+cmake --version
+
+BUILD_DIR=$(dirname $(realpath "$0"))/Catch2/build
+echo -e "\nThe build directory is ${BUILD_DIR}.\n"
+
+echo -e "Installing Debug configuration.\n"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF "$@"
+cmake --build $BUILD_DIR --config Debug --parallel
+cmake --install $BUILD_DIR --config Debug
+
+echo -e "Installing RelWithDebInfo configuration.\n"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=OFF "$@"
+cmake --build $BUILD_DIR --config RelWithDebInfo --parallel
+cmake --install $BUILD_DIR --config RelWithDebInfo
+
+echo -e "Installing Release configuration.\n"
+cmake -S $BUILD_DIR/.. -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF "$@"
+cmake --build $BUILD_DIR --config Release --parallel
+cmake --install $BUILD_DIR --config Release
