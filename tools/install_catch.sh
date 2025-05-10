77f7ae8406f7b77af7054af304bcdeba97401461
KRM7 <70973547+KRM7@users.noreply.github.com>
2025-05-10 12:14:10 +0200

    add tests for reproducibility

--- tools/install_catch.sh
+++ tools/install_catch.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 echo -e "Installing Catch2...\n"
 
@@ -11,17 +11,7 @@ git clone -b v3.3.0 https://github.com/catchorg/Catch2.git $BUILD_DIR/Catch2
 CATCH_BUILD_DIR=$BUILD_DIR/Catch2/build
 echo -e "\nThe Catch build directory is ${CATCH_BUILD_DIR}.\n"
 
-echo -e "Installing Debug configuration.\n"
-cmake -S $CATCH_BUILD_DIR/.. -B $CATCH_BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=OFF "$@"
-cmake --build $CATCH_BUILD_DIR --config Debug --parallel
-cmake --install $CATCH_BUILD_DIR --config Debug
-
-echo -e "Installing RelWithDebInfo configuration.\n"
-cmake -S $CATCH_BUILD_DIR/.. -B $CATCH_BUILD_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTING=OFF "$@"
-cmake --build $CATCH_BUILD_DIR --config RelWithDebInfo --parallel
-cmake --install $CATCH_BUILD_DIR --config RelWithDebInfo
-
-echo -e "Installing Release configuration.\n"
-cmake -S $CATCH_BUILD_DIR/.. -B $CATCH_BUILD_DIR -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF "$@"
-cmake --build $CATCH_BUILD_DIR --config Release --parallel
-cmake --install $CATCH_BUILD_DIR --config Release
+echo -e "Installing $1 configuration.\n"
+cmake -S $CATCH_BUILD_DIR/.. -B $CATCH_BUILD_DIR -DCMAKE_BUILD_TYPE=$1 -DBUILD_TESTING=OFF "${@:2}"
+cmake --build $CATCH_BUILD_DIR --config $1 --parallel
+cmake --install $CATCH_BUILD_DIR --config $1
