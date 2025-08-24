843caac32afab7f3fc9b5c7d8860a88307064591
KRM7 <70973547+KRM7@users.noreply.github.com>
2025-08-24 20:34:18 +0200

    extend dynamic_bitset interface

--- src/utility/dynamic_bitset.hpp
+++ src/utility/dynamic_bitset.hpp
@@ -4,27 +4,42 @@
 #define GAPP_UTILITY_DYNAMIC_BITSET_HPP
 
 #include "small_vector.hpp"
+#include "iterators.hpp"
 #include "bit.hpp"
+#include "hash.hpp"
 #include "utility.hpp"
+#include <algorithm>
+#include <iterator>
 #include <memory>
 #include <bit>
+#include <compare>
 #include <limits>
 #include <cstdint>
 #include <cstddef>
 
 // NOLINTBEGIN(*bool-conversion, *assignment, *assignment-signature, *operator, *ref-data-members)
 
-namespace gapp::detail
+namespace gapp
 {
-    class dynamic_bitset
+    class dynamic_bitset : public detail::iterator_interface<dynamic_bitset>
     {
     public:
         using value_type      = bool;
         using block_type      = std::size_t;
         using allocator_type  = std::allocator<block_type>;
         using const_reference = bool;
+        using pointer         = void;
+        using const_pointer   = void;
         using size_type       = std::size_t;
         using difference_type = std::ptrdiff_t;
+
+        class reference;
+
+        using iterator       = detail::stable_iterator<dynamic_bitset>;
+        using const_iterator = detail::const_stable_iterator<dynamic_bitset>;
+
+        using reverse_iterator       = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
         static constexpr size_type block_size = std::numeric_limits<block_type>::digits;
 
@@ -47,9 +62,47 @@ namespace gapp::detail
                 return *this = static_cast<bool>(rhs);
             }
 
+            constexpr const reference& operator&=(bool value) const noexcept
+            {
+                if (!value) clear();
+                return *this;
+            }
+
+            constexpr const reference& operator|=(bool value) const noexcept
+            {
+                if (value) set();
+                return *this;
+            }
+
+            constexpr const reference& operator^=(bool value) const noexcept
+            {
+                if (value) flip();
+                return *this;
+            }
+
+            constexpr const reference& operator-=(bool value) const noexcept
+            {
+                if (value) clear();
+                return *this;
+            }
+
             constexpr void set() const noexcept   { block_ |= mask_; }
             constexpr void clear() const noexcept { block_ &= ~mask_; }
             constexpr void flip() const noexcept  { block_ ^= mask_; }
+
+            constexpr void swap(const reference& other) const noexcept
+            {
+                if (*this != other)
+                {
+                    this->flip();
+                    other.flip();
+                }
+            }
+
+            constexpr friend void swap(const reference& lhs, const reference& rhs) noexcept
+            {
+                lhs.swap(rhs);
+            }
             
             constexpr void operator&() = delete;
 
@@ -69,13 +122,13 @@ namespace gapp::detail
 
         constexpr dynamic_bitset() = default;
 
-        constexpr dynamic_bitset(size_type size) :
+        constexpr explicit dynamic_bitset(size_type size) :
             blocks_(size / block_size + bool(size % block_size)),
             size_(size)
         {}
 
         constexpr dynamic_bitset(size_type size, bool value) :
-            blocks_(size / block_size + bool(size % block_size), block_of<block_type>(value)),
+            blocks_(size / block_size + bool(size % block_size), detail::block_of<block_type>(value)),
             size_(size)
         {}
 
@@ -101,6 +154,12 @@ namespace gapp::detail
             return static_cast<bool>(blocks_[block] & (block_type(1) << offset));
         }
 
+        constexpr iterator begin() noexcept { return { this, 0 }; }
+        constexpr iterator end() noexcept { return { this, size_ }; }
+
+        constexpr const_iterator begin() const noexcept { return { this, 0 }; }
+        constexpr const_iterator end() const noexcept { return { this, size_ }; }
+
         constexpr bool empty() const noexcept
         {
             return size_ == 0;
@@ -119,22 +178,29 @@ namespace gapp::detail
 
         constexpr void resize(size_type new_size, bool value = false)
         {
-            blocks_.resize(new_size / block_size + bool(new_size % block_size), block_of<block_type>(value));
+            blocks_.resize((new_size + block_size) / block_size, detail::block_of<block_type>(value));
 
             if (new_size > size_)
             {
                 blocks_.back() &= partial_block_mask();
-                blocks_.back() |= ~partial_block_mask() & block_of<block_type>(value);
+                blocks_.back() |= ~partial_block_mask() & detail::block_of<block_type>(value);
             }
 
             size_ = new_size;
+        }
+
+        constexpr void push_back(bool value)
+        {
+            if (size_ % block_size == 0) blocks_.push_back(block_type{ 0 });
+
+            (*this)[size_++] = value;
         }
 
         constexpr void fill(bool value) noexcept
         {
             for (block_type& block : blocks_)
             {
-                block = block_of<block_type>(value);
+                block = detail::block_of<block_type>(value);
             }
         }
 
@@ -159,17 +225,17 @@ namespace gapp::detail
         {
             for (size_type i = 0; i < full_block_count(); i++)
             {
-                if (blocks_[i] != zeros<block_type>) return true;
+                if (blocks_[i] != detail::zeros<block_type>) return true;
             }
 
-            return partial_block() != zeros<block_type>;
+            return partial_block() != detail::zeros<block_type>;
         }
 
         constexpr bool all_set() const noexcept
         {
             for (size_type i = 0; i < full_block_count(); i++)
             {
-                if (blocks_[i] != ones<block_type>) return false;
+                if (blocks_[i] != detail::ones<block_type>) return false;
             }
 
             return size_type(std::popcount(partial_block())) == partial_block_size();
@@ -180,16 +246,25 @@ namespace gapp::detail
             return !any_set();
         }
 
-        constexpr friend dynamic_bitset operator~(const dynamic_bitset& bitset) noexcept
+        constexpr std::span<block_type> blocks() noexcept
         {
-            dynamic_bitset complement(bitset.size());
+            return blocks_;
+        }
 
-            for (size_type i = 0; i < bitset.blocks_.size(); i++)
-            {
-                complement.blocks_[i] = ~bitset.blocks_[i];
-            }
+        constexpr std::span<const block_type> blocks() const noexcept
+        {
+            return blocks_;
+        }
 
-            return complement;
+        constexpr void swap(dynamic_bitset& other) noexcept
+        {
+            blocks_.swap(other.blocks_);
+            std::swap(size_, other.size_);
+        }
+
+        constexpr friend void swap(dynamic_bitset& lhs, dynamic_bitset& rhs) noexcept
+        {
+            lhs.swap(rhs);
         }
 
         constexpr friend bool operator==(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
@@ -204,6 +279,164 @@ namespace gapp::detail
             return lhs.partial_block() == rhs.partial_block();
         }
 
+        constexpr friend auto operator<=>(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            const size_type block_count = std::min(lhs.full_block_count(), rhs.full_block_count());
+
+            for (size_t i = 0; i < block_count; i++)
+            {
+                const block_type lhs_block = lhs.blocks_[i];
+                const block_type rhs_block = rhs.blocks_[i];
+
+                if (lhs_block != rhs_block) return lhs_block <=> rhs_block;
+            }
+
+            if (lhs.size() < rhs.size())
+            {
+                const block_type lhs_block = lhs.partial_block();
+                const block_type rhs_block = rhs.blocks_[block_count] & lhs.partial_block_mask();
+
+                if (lhs_block != rhs_block) return lhs_block <=> rhs_block;
+
+                return std::strong_ordering::less;
+            }
+
+            if (lhs.size() > rhs.size())
+            {
+                const block_type lhs_block = lhs.blocks_[block_count] & rhs.partial_block_mask();
+                const block_type rhs_block = rhs.partial_block();
+
+                if (lhs_block != rhs_block) return lhs_block <=> rhs_block;
+
+                return std::strong_ordering::greater;
+            }
+
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            return lhs.partial_block() <=> rhs.partial_block();
+        }
+
+        constexpr friend dynamic_bitset operator~(const dynamic_bitset& bitset) noexcept
+        {
+            dynamic_bitset complement(bitset.size());
+
+            for (size_type i = 0; i < bitset.blocks_.size(); i++)
+            {
+                complement.blocks_[i] = ~bitset.blocks_[i];
+            }
+
+            return complement;
+        }
+
+        constexpr void flip() noexcept
+        {
+            for (block_type& block : blocks_) { block = ~block; }
+        }
+
+        constexpr friend dynamic_bitset operator&(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            dynamic_bitset result(lhs.size());
+
+            for (size_type i = 0; i < result.blocks_.size(); i++)
+            {
+                result.blocks_[i] = lhs.blocks_[i] & rhs.blocks_[i];
+            }
+
+            return result;
+        }
+
+        constexpr friend dynamic_bitset& operator&=(dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            for (size_type i = 0; i < lhs.blocks_.size(); i++)
+            {
+                lhs.blocks_[i] &= rhs.blocks_[i];
+            }
+
+            return lhs;
+        }
+
+        constexpr friend dynamic_bitset operator|(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            dynamic_bitset result(lhs.size());
+
+            for (size_type i = 0; i < result.blocks_.size(); i++)
+            {
+                result.blocks_[i] = lhs.blocks_[i] | rhs.blocks_[i];
+            }
+
+            return result;
+        }
+
+        constexpr friend dynamic_bitset& operator|=(dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            for (size_type i = 0; i < lhs.blocks_.size(); i++)
+            {
+                lhs.blocks_[i] |= rhs.blocks_[i];
+            }
+
+            return lhs;
+        }
+
+        constexpr friend dynamic_bitset operator^(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            dynamic_bitset result(lhs.size());
+
+            for (size_type i = 0; i < result.blocks_.size(); i++)
+            {
+                result.blocks_[i] = lhs.blocks_[i] ^ rhs.blocks_[i];
+            }
+
+            return result;
+        }
+
+        constexpr friend dynamic_bitset& operator^=(dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            for (size_type i = 0; i < lhs.blocks_.size(); i++)
+            {
+                lhs.blocks_[i] ^= rhs.blocks_[i];
+            }
+
+            return lhs;
+        }
+
+        constexpr friend dynamic_bitset operator-(const dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            dynamic_bitset result(lhs.size());
+
+            for (size_type i = 0; i < result.blocks_.size(); i++)
+            {
+                result.blocks_[i] = lhs.blocks_[i] & ~rhs.blocks_[i];
+            }
+
+            return result;
+        }
+
+        constexpr friend dynamic_bitset& operator-=(dynamic_bitset& lhs, const dynamic_bitset& rhs) noexcept
+        {
+            GAPP_ASSERT(lhs.size() == rhs.size());
+
+            for (size_type i = 0; i < lhs.blocks_.size(); i++)
+            {
+                lhs.blocks_[i] &= ~rhs.blocks_[i];
+            }
+
+            return lhs;
+        }
+
     private:
         small_vector<block_type, 4> blocks_;
         size_type size_ = 0;
@@ -212,7 +445,7 @@ namespace gapp::detail
         {
             for (size_type i = 0; i < blocks_.size(); i++)
             {
-                if (blocks_[i] == zeros<block_type>) continue;
+                if (blocks_[i] == detail::zeros<block_type>) continue;
                 return std::min(size_, i * block_size + std::countr_zero(blocks_[i]));
             }
             return size_;
@@ -222,7 +455,7 @@ namespace gapp::detail
         {
             for (size_type i = 0; i < blocks_.size(); i++)
             {
-                if (blocks_[i] == ones<block_type>) continue;
+                if (blocks_[i] == detail::ones<block_type>) continue;
                 return std::min(size_, i * block_size + std::countr_one(blocks_[i]));
             }
             return size_;
@@ -240,16 +473,29 @@ namespace gapp::detail
 
         constexpr block_type partial_block_mask() const noexcept
         {
-            return mask_right_n<block_type>(partial_block_size());
+            return detail::mask_right_n<block_type>(partial_block_size());
         }
 
         constexpr block_type partial_block() const noexcept
         {
-            return blocks_.back() & partial_block_mask();
+            return !empty() ? blocks_.back() & partial_block_mask() : block_type{ 0 };
         }
     };
 
-} // namespace gapp::detail
+} // namespace gapp
+
+namespace std
+{
+    template<>
+    struct hash<gapp::dynamic_bitset>
+    {
+        std::size_t operator()(const gapp::dynamic_bitset& bitset) const noexcept
+        {
+            return gapp::detail::hash_range(bitset.blocks());
+        }
+    };
+
+} // namespace std
 
 // NOLINTEND(*bool-conversion, *assignment, *assignment-signature, *operator, *ref-data-members)
 
--- test/unit/dynamic_bitset.cpp
+++ test/unit/dynamic_bitset.cpp
@@ -2,9 +2,10 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include "utility/dynamic_bitset.hpp"
+#include <algorithm>
 #include <utility>
 
-using gapp::detail::dynamic_bitset;
+using gapp::dynamic_bitset;
 
 
 TEST_CASE("constructor_default", "[dynamic_bitset]")
@@ -84,6 +85,15 @@ TEST_CASE("resize", "[dynamic_bitset]")
     REQUIRE(bitset[98] == true);
     REQUIRE(bitset[99] == false);
     REQUIRE(bitset[100] == true);
+
+    bitset.resize(256, true);
+
+    REQUIRE(bitset[255] == true);
+
+    dynamic_bitset empty;
+    empty.resize(31, true);
+
+    REQUIRE(empty[30] == true);
 }
 
 TEST_CASE("find_first_true", "[dynamic_bitset]")
@@ -99,6 +109,8 @@ TEST_CASE("find_first_true", "[dynamic_bitset]")
 
     bitset[0] = true;
     REQUIRE(bitset.find_first(true) == 0);
+
+    REQUIRE(dynamic_bitset{}.find_first(true) == 0);
 }
 
 TEST_CASE("find_first_false", "[dynamic_bitset]")
@@ -114,6 +126,8 @@ TEST_CASE("find_first_false", "[dynamic_bitset]")
 
     bitset[0] = false;
     REQUIRE(bitset.find_first(false) == 0);
+
+    REQUIRE(dynamic_bitset{}.find_first(false) == 0);
 }
 
 TEST_CASE("popcount", "[dynamic_bitset]")
@@ -132,6 +146,14 @@ TEST_CASE("popcount", "[dynamic_bitset]")
 
     bitset[99] = true;
     REQUIRE(bitset.popcount() == 2);
+
+    bitset.resize(64);
+    bitset.fill(true);
+
+    REQUIRE(bitset.popcount() == 64);
+
+    dynamic_bitset empty;
+    REQUIRE(empty.popcount() == 0);
 }
 
 TEST_CASE("fill", "[dynamic_bitset]")
@@ -152,6 +174,8 @@ TEST_CASE("any_set", "[dynamic_bitset]")
 
     bitset[99] = true;
     REQUIRE(bitset.any_set());
+
+    REQUIRE(!dynamic_bitset{}.any_set());
 }
 
 TEST_CASE("all_set", "[dynamic_bitset]")
@@ -164,6 +188,8 @@ TEST_CASE("all_set", "[dynamic_bitset]")
 
     bitset.fill(true);
     REQUIRE(bitset.all_set());
+
+    REQUIRE(dynamic_bitset{}.all_set());
 }
 
 TEST_CASE("none_set", "[dynamic_bitset]")
@@ -173,6 +199,8 @@ TEST_CASE("none_set", "[dynamic_bitset]")
 
     bitset[99] = true;
     REQUIRE(!bitset.none_set());
+
+    REQUIRE(dynamic_bitset{}.none_set());
 }
 
 TEST_CASE("operator~", "[dynamic_bitset]")
@@ -182,4 +210,403 @@ TEST_CASE("operator~", "[dynamic_bitset]")
 
     dynamic_bitset bitset2 = ~bitset1;
     REQUIRE(bitset2.popcount() == 100);
+}
+
+TEST_CASE("flip", "[dynamic_bitset]")
+{
+    dynamic_bitset bitset(100, false);
+    bitset[17] = true;
+
+    REQUIRE(bitset.popcount() == 1);
+
+    bitset.flip();
+    REQUIRE(bitset.popcount() == 99);
+}
+
+TEST_CASE("operator&", "[dynamic_bitset]")
+{
+    const dynamic_bitset all_ones(100, true);
+    const dynamic_bitset all_zeros(100, false);
+
+    REQUIRE((all_ones & all_zeros) == all_zeros);
+    REQUIRE((all_zeros & all_ones) == all_zeros);
+    REQUIRE((all_ones & all_ones) == all_ones);
+    REQUIRE((all_zeros & all_zeros) == all_zeros);
+}
+
+TEST_CASE("operator&=", "[dynamic_bitset]")
+{
+    dynamic_bitset all_ones(100, true);
+    dynamic_bitset all_zeros(100, false);
+
+    all_ones &= all_ones;
+    REQUIRE(all_ones.popcount() == 100);
+
+    all_zeros &= all_zeros;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_zeros &= all_ones;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_ones &= all_zeros;
+    REQUIRE(all_ones.popcount() == 0);
+}
+
+TEST_CASE("operator|", "[dynamic_bitset]")
+{
+    const dynamic_bitset all_ones(100, true);
+    const dynamic_bitset all_zeros(100, false);
+
+    REQUIRE((all_ones | all_zeros) == all_ones);
+    REQUIRE((all_zeros | all_ones) == all_ones);
+    REQUIRE((all_ones | all_ones) == all_ones);
+    REQUIRE((all_zeros | all_zeros) == all_zeros);
+}
+
+TEST_CASE("operator|=", "[dynamic_bitset]")
+{
+    dynamic_bitset all_ones(100, true);
+    dynamic_bitset all_zeros(100, false);
+
+    all_ones |= all_ones;
+    REQUIRE(all_ones.popcount() == 100);
+
+    all_zeros |= all_zeros;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_ones |= all_zeros;
+    REQUIRE(all_ones.popcount() == 100);
+
+    all_zeros |= all_ones;
+    REQUIRE(all_zeros.popcount() == 100);
+}
+
+TEST_CASE("operator^", "[dynamic_bitset]")
+{
+    const dynamic_bitset all_ones(100, true);
+    const dynamic_bitset all_zeros(100, false);
+
+    REQUIRE((all_ones ^ all_zeros) == all_ones);
+    REQUIRE((all_zeros ^ all_ones) == all_ones);
+    REQUIRE((all_ones ^ all_ones) == all_zeros);
+    REQUIRE((all_zeros ^ all_zeros) == all_zeros);
+}
+
+TEST_CASE("operator^=", "[dynamic_bitset]")
+{
+    dynamic_bitset all_ones(100, true);
+    dynamic_bitset all_zeros(100, false);
+
+    all_zeros ^= all_zeros;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_ones ^= all_zeros;
+    REQUIRE(all_ones.popcount() == 100);
+
+    all_ones ^= all_ones;
+    REQUIRE(all_ones.popcount() == 0);
+
+    all_ones.fill(true);
+
+    all_zeros ^= all_ones;
+    REQUIRE(all_zeros.popcount() == 100);
+}
+
+TEST_CASE("operator-", "[dynamic_bitset]")
+{
+    const dynamic_bitset all_ones(100, true);
+    const dynamic_bitset all_zeros(100, false);
+
+    REQUIRE((all_ones - all_zeros) == all_ones);
+    REQUIRE((all_zeros - all_ones) == all_zeros);
+    REQUIRE((all_ones - all_ones) == all_zeros);
+    REQUIRE((all_zeros - all_zeros) == all_zeros);
+}
+
+TEST_CASE("operator-=", "[dynamic_bitset]")
+{
+    dynamic_bitset all_ones(100, true);
+    dynamic_bitset all_zeros(100, false);
+
+    all_zeros -= all_zeros;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_ones -= all_zeros;
+    REQUIRE(all_ones.popcount() == 100);
+
+    all_zeros -= all_ones;
+    REQUIRE(all_zeros.popcount() == 0);
+
+    all_ones -= all_ones;
+    REQUIRE(all_ones.popcount() == 0);
+}
+
+TEST_CASE("swap", "[dynamic_bitset]")
+{
+    dynamic_bitset all_ones(100, true);
+    dynamic_bitset all_zeros(50, false);
+
+    all_ones.swap(all_zeros);
+
+    REQUIRE(all_ones.size() == 50);
+    REQUIRE(all_ones.popcount() == 0);
+
+    REQUIRE(all_zeros.size() == 100);
+    REQUIRE(all_zeros.popcount() == 100);
+
+    all_zeros.swap(all_ones);
+
+    REQUIRE(all_ones.size() == 100);
+    REQUIRE(all_ones.popcount() == 100);
+
+    REQUIRE(all_zeros.size() == 50);
+    REQUIRE(all_zeros.popcount() == 0);
+
+    using std::swap;
+    swap(all_ones, all_zeros);
+
+    REQUIRE(all_ones.size() == 50);
+    REQUIRE(all_ones.popcount() == 0);
+
+    REQUIRE(all_zeros.size() == 100);
+    REQUIRE(all_zeros.popcount() == 100);
+
+    swap(all_ones, all_zeros);
+
+    REQUIRE(all_ones.size() == 100);
+    REQUIRE(all_ones.popcount() == 100);
+
+    REQUIRE(all_zeros.size() == 50);
+    REQUIRE(all_zeros.popcount() == 0);
+}
+
+TEST_CASE("hash", "[dynamic_bitset]")
+{
+    const dynamic_bitset bitset(100, true);
+
+    std::hash<dynamic_bitset>{}(bitset);
+    SUCCEED();
+}
+
+TEST_CASE("reference_copy", "[dynamic_bitset]")
+{
+    dynamic_bitset bitset(100, false);
+
+    const auto ref = bitset[10];
+    const auto ref_copy = ref;
+
+    ref_copy = true;
+
+    REQUIRE(ref == true);
+    REQUIRE(bitset[10] == true);
+}
+
+TEST_CASE("reference_operator&=", "[dynamic_bitset]")
+{
+    dynamic_bitset ones(100, true);
+    dynamic_bitset zeros(100, false);
+
+    REQUIRE(ones[10] & ones[2]);
+    REQUIRE(ones[10] & true);
+
+    REQUIRE((ones[10] &= ones[9]));
+    REQUIRE(!(ones[10] &= false));
+}
+
+TEST_CASE("reference_operator|=", "[dynamic_bitset]")
+{
+    dynamic_bitset ones(100, true);
+    dynamic_bitset zeros(100, false);
+
+    REQUIRE(ones[10] | zeros[2]);
+    REQUIRE(ones[10] | false);
+
+    REQUIRE(!(zeros[10] |= false));
+    REQUIRE((zeros[10] |= ones[9]));
+}
+
+TEST_CASE("reference_operator^=", "[dynamic_bitset]")
+{
+    dynamic_bitset ones(100, true);
+    dynamic_bitset zeros(100, false);
+
+    REQUIRE(ones[10] ^ zeros[2]);
+    REQUIRE(ones[10] ^ false);
+
+    REQUIRE(!(zeros[10] ^= false));
+    REQUIRE((zeros[10] ^= ones[9]));
+}
+
+TEST_CASE("reference_operator-=", "[dynamic_bitset]")
+{
+    dynamic_bitset ones(100, true);
+    dynamic_bitset zeros(100, false);
+
+    REQUIRE(ones[10] - zeros[2]);
+    REQUIRE(ones[10] - false);
+
+    REQUIRE(!(zeros[10] -= true));
+    REQUIRE(!(zeros[10] -= false));
+    REQUIRE(!(ones[10] -= ones[9]));
+}
+
+TEST_CASE("reference_swap", "[dynamic_bitset]")
+{
+    dynamic_bitset ones(100, true);
+    dynamic_bitset zeros(100, false);
+
+    ones[10].swap(zeros[0]);
+
+    REQUIRE(!ones[10]);
+    REQUIRE(zeros[0]);
+
+    ones[10].swap(ones[9]);
+
+    REQUIRE(ones[10]);
+    REQUIRE(!ones[9]);
+
+    using std::swap;
+    swap(ones[9], ones[10]);
+
+    REQUIRE(ones[9]);
+    REQUIRE(!ones[10]);
+
+    swap(ones[32], ones[33]);
+
+    REQUIRE(ones[32]);
+    REQUIRE(ones[33]);
+}
+
+TEST_CASE("push_back", "[dynamic_bitset]")
+{
+    dynamic_bitset bitset;
+
+    REQUIRE(bitset.empty());
+
+    bitset.push_back(false);
+    bitset.push_back(true);
+    bitset.push_back(false);
+
+    REQUIRE(bitset.size() == 3);
+
+    REQUIRE(bitset[0] == false);
+    REQUIRE(bitset[1] == true);
+    REQUIRE(bitset[2] == false);
+
+    bitset.resize(128);
+    bitset.push_back(true);
+
+    REQUIRE(bitset[128] == true);
+}
+
+TEST_CASE("equality_comparison", "[dynamic_bitset]")
+{
+    {
+        dynamic_bitset bitset1;
+
+        REQUIRE(bitset1 == bitset1);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, true);
+
+        REQUIRE(bitset1 == bitset2);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, false);
+
+        REQUIRE(bitset1 != bitset2);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, true);
+
+        bitset2[99] = false;
+
+        REQUIRE(bitset1 != bitset2);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(101, true);
+
+        REQUIRE(bitset1 != bitset2);
+    }
+}
+
+TEST_CASE("three_way_comparison", "[dynamic_bitset]")
+{
+    {
+        dynamic_bitset bitset1;
+
+        REQUIRE((bitset1 <=> bitset1) == std::strong_ordering::equal);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, true);
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::equal);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::equal);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, false);
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::greater);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::less);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(256, false);
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::greater);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::less);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(100, true);
+        bitset2[98] = false;
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::greater);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::less);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(101, true);
+        bitset2[100] = false;
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::less);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::greater);
+    }
+
+    {
+        dynamic_bitset bitset1(100, true);
+        dynamic_bitset bitset2(101, true);
+
+        REQUIRE((bitset1 <=> bitset2) == std::strong_ordering::less);
+        REQUIRE((bitset2 <=> bitset1) == std::strong_ordering::greater);
+    }
+}
+
+TEST_CASE("iterators", "[dynamic_bitset]")
+{
+    dynamic_bitset bitset(100, false);
+
+    std::transform(bitset.begin(), bitset.end(), bitset.begin(),
+    [cnt = 0](dynamic_bitset::reference) mutable
+    {
+        return bool(cnt++ % 2);
+    });
+
+    REQUIRE(std::count(bitset.crbegin(), bitset.crend(), true) == 50);
+    REQUIRE(bitset.popcount() == 50);
 }
